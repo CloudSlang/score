f023fe28bd5ad036806b3ce47e18f88a5b1ae289
Dima Rassin <dima.rassin@hp.com>
2014-01-02 09:53:14 +0200

    change maven version

--- engine/versioning/oo-versioning-impl/src/test/java/com/hp/oo/engine/versioning/repositories/VersionRepositoryTest.java
+++ engine/versioning/oo-versioning-impl/src/test/java/com/hp/oo/engine/versioning/repositories/VersionRepositoryTest.java
@@ -3,18 +3,17 @@ package com.hp.oo.engine.versioning.repositories;
 import com.hp.oo.engine.versioning.entities.VersionCounter;
 import com.hp.score.engine.data.SimpleHiloIdentifierGenerator;
 import junit.framework.Assert;
+import liquibase.integration.spring.SpringLiquibase;
 import org.apache.commons.dbcp.BasicDataSource;
-import org.hibernate.ejb.HibernatePersistence;
+import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.MethodInvokingFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.data.jpa.repository.support.JpaRepositoryFactory;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -26,14 +25,11 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 import javax.sql.DataSource;
-import java.io.IOException;
-import java.io.StringReader;
 import java.util.Properties;
 
 /**
@@ -47,6 +43,8 @@ import java.util.Properties;
 @TransactionConfiguration(defaultRollback = true)
 public class VersionRepositoryTest {
 
+	private static final boolean SHOW_SQL = false;
+
 	@Autowired
 	private VersionRepository repository;
 
@@ -55,125 +53,93 @@ public class VersionRepositoryTest {
 
 	@Test
 	public void testRecoveryVersionCounter() {
+		String counterName = "my counter";
+		repository.save(new VersionCounter(counterName));
+		repository.flush();
+
 		//MSG_RECOVERY_VERSION is created in liquibase files, making sure it there...
-		VersionCounter result = repository.findByCounterName("MSG_RECOVERY_VERSION");
+		VersionCounter result = repository.findByCounterName(counterName);
 		Assert.assertEquals(0L, result.getVersionCount());
 
-		int resultRowCount = repository.incrementCounterByName("MSG_RECOVERY_VERSION");
+		int resultRowCount = repository.incrementCounterByName(counterName);
 		Assert.assertEquals(1, resultRowCount);
 
 		em.refresh(result);  //refresh from first level cache
-		result = repository.findByCounterName("MSG_RECOVERY_VERSION");
+		result = repository.findByCounterName(counterName);
 		Assert.assertEquals(1L, result.getVersionCount());
 	}
 
 
 	@Configuration
+	@EnableJpaRepositories("com.hp.oo.engine.versioning.repositories")
 	@EnableTransactionManagement
 	static class Configurator {
+		@Autowired
+		private DataSource dataSource;
 
 		@Autowired
-		private JpaRepositoryFactory repositoryFactory;
+		private JpaVendorAdapter jpaVendorAdapter;
+
+		@Autowired
+		private EntityManagerFactory emf;
 
 		@Bean
-		VersionRepository repository() {
-			return repositoryFactory.getRepository(VersionRepository.class);
+		DataSource dataSource() {
+			BasicDataSource ds = new BasicDataSource();
+			ds.setDriverClassName("org.h2.Driver");
+			ds.setUrl("jdbc:h2:mem:test");
+			ds.setUsername("sa");
+			ds.setPassword("sa");
+			ds.setDefaultAutoCommit(false);
+			return new TransactionAwareDataSourceProxy(ds);
 		}
 
-		@Configuration
-		@EnableTransactionManagement
-		static class EMFContext {
-			@Autowired
-			EntityManagerFactory emf;
-			@PersistenceContext
-			EntityManager entityManager;
-
-
-			@Bean
-			Properties hibernateProperties() {
-				Properties properties = new Properties();
-				try {
-					properties.load(new StringReader(
-							"hibernate.format_sql=true" + "\n" +
-									"hibernate.cache.provider_class=org.hibernate.cache.NoCacheProvider" + "\n" +
-									"hibernate.cache.use_query_cache=false" + "\n" +
-									"hibernate.generate_statistics=false" + "\n" +
-									"hibernate.cache.use_second_level_cache=false" + "\n" +
-									"hibernate.jdbc.fetch_size=100" + "\n" +
-									"hibernate.jdbc.batch_size=30" + "\n" +
-									"hibernate.order_updates=true" + "\n" +
-									"hibernate.order_inserts=true" + "\n" +
-									"hibernate.default_batch_fetch_size=20" + "\n" +
-									"hibernate.hbm2ddl.auto=validate" + "\n" +
-									"hibernate.dialect_resolvers=org.hibernate.service.jdbc.dialect.internal.StandardDialectResolver"
-					));
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-				return properties;
-			}
-
-			@Bean
-			DataSource dataSource() {
-				BasicDataSource ds = new BasicDataSource();
-				ds.setDriverClassName("org.h2.Driver");
-				ds.setUrl("jdbc:h2:mem:test");
-				ds.setUsername("sa");
-				ds.setPassword("sa");
-				ds.setDefaultAutoCommit(false);
-				return new TransactionAwareDataSourceProxy(ds);
-			}
-
-			@Bean(name = "initHiloGenerator")
-			MethodInvokingFactoryBean initHiloGenerator() {
-				MethodInvokingFactoryBean methodInvokingFactoryBean = new MethodInvokingFactoryBean();
-				methodInvokingFactoryBean.setTargetClass(SimpleHiloIdentifierGenerator.class);
-				methodInvokingFactoryBean.setTargetMethod("setDataSource");
-				methodInvokingFactoryBean.setArguments(new Object[]{dataSource()});
-				return methodInvokingFactoryBean;
-			}
-
-			@Bean
-			JpaVendorAdapter jpaVendorAdapter() {
-				HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-				adapter.setShowSql(false);
-				adapter.setGenerateDdl(true);
-				return adapter;
-			}
-
-			@Bean
-			@DependsOn({"liquibase", "initHiloGenerator"})
-			FactoryBean<EntityManagerFactory> emf() {
-				LocalContainerEntityManagerFactoryBean fb = new LocalContainerEntityManagerFactoryBean();
-				fb.setJpaProperties(hibernateProperties());
-				fb.setDataSource(dataSource());
-				fb.setPersistenceProviderClass(HibernatePersistence.class);
-				fb.setPackagesToScan("com.hp.oo.engine.versioning");
-				fb.setJpaVendorAdapter(jpaVendorAdapter());
-				return fb;
-			}
-
-			@Bean
-			PlatformTransactionManager transactionManager() throws Exception {
-				return new JpaTransactionManager(emf);
-			}
-
-			@Bean
-			TransactionTemplate createTransactionTemplate() throws Exception {
-				return new TransactionTemplate(transactionManager());
-			}
-
-			@Bean
-			JdbcTemplate jdbcTemplate() {
-				return new JdbcTemplate(dataSource());
-			}
-
-			@Bean
-			JpaRepositoryFactory artifactRepositoryFactory() throws Exception {
-				return new JpaRepositoryFactory(entityManager);
-			}
-
+		@Bean
+		SpringLiquibase liquibase() {
+			SpringLiquibase liquibase = new SpringLiquibase();
+			liquibase.setDataSource(dataSource);
+			liquibase.setChangeLog("classpath:/META-INF/database/test.changes.xml");
+			SimpleHiloIdentifierGenerator.setDataSource(dataSource);
+			return liquibase;
 		}
 
+
+		@Bean
+		Properties hibernateProperties() {
+			return new Properties(){{
+				setProperty("hibernate.format_sql", "true");
+				setProperty("hibernate.hbm2ddl.auto", "create-drop");
+				setProperty("hibernate.cache.use_query_cache", "false");
+				setProperty("hibernate.generate_statistics", "false");
+				setProperty("hibernate.cache.use_second_level_cache", "false");
+				setProperty("hibernate.order_updates", "true");
+				setProperty("hibernate.order_inserts", "true");
+			}};
+		}
+
+		@Bean
+		JpaVendorAdapter jpaVendorAdapter() {
+			HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+			adapter.setShowSql(SHOW_SQL);
+			adapter.setGenerateDdl(true);
+			return adapter;
+		}
+
+		@Bean(name="entityManagerFactory")
+		@DependsOn("liquibase")
+		FactoryBean<EntityManagerFactory> emf() {
+			LocalContainerEntityManagerFactoryBean fb = new LocalContainerEntityManagerFactoryBean();
+			fb.setJpaProperties(hibernateProperties());
+			fb.setDataSource(dataSource());
+			fb.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+			fb.setPackagesToScan("com.hp.oo.engine.versioning");
+			fb.setJpaVendorAdapter(jpaVendorAdapter);
+			return fb;
+		}
+
+		@Bean
+		PlatformTransactionManager transactionManager() throws Exception {
+			return new JpaTransactionManager(emf);
+		}
 	}
 }
