1306e50594f9c6cf9b764891afecfffcd61b826a
wahnonm <meir.wahnon@hp.com>
2014-02-25 11:43:31 +0200

    not sending the execution events on the Bus...

--- worker/execution/oo-execution-impl/src/main/java/com/hp/oo/execution/services/ExecutionServiceImpl.java
+++ worker/execution/oo-execution-impl/src/main/java/com/hp/oo/execution/services/ExecutionServiceImpl.java
@@ -498,9 +498,9 @@ public final class ExecutionServiceImpl implements ExecutionService {
 
     private void dumpEvents(Execution execution) {
         List<ExecutionEvent> executionEvents = execution.getAggregatedEvents();
-        for (ExecutionEvent executionEvent:executionEvents){
-            eventBus.dispatch(new EventWrapper(executionEvent.getType().name(), executionEvent));
-        }
+        //for (ExecutionEvent executionEvent:executionEvents){
+            //eventBus.dispatch(new EventWrapper(executionEvent.getType().name(), executionEvent));
+        //}
 
         List<ExecutionEvent> filteredExecutionEvents = new ArrayList<>();
         for (ExecutionEvent executionEvent : executionEvents){
