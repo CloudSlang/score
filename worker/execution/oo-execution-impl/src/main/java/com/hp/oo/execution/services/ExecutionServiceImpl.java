73663acc44ff441a4740a07ba86636e1ce9e9399
wahnonm <meir.wahnon@hp.com>
2014-05-11 17:31:21 +0300

    remove unneeded serialization of events

--- worker/execution/oo-execution-impl/src/main/java/com/hp/oo/execution/services/ExecutionServiceImpl.java
+++ worker/execution/oo-execution-impl/src/main/java/com/hp/oo/execution/services/ExecutionServiceImpl.java
@@ -459,6 +459,7 @@ public final class ExecutionServiceImpl implements ExecutionService {
     }
 
     private void addExecutionEvent(Execution execution) {
+        if(!eventsPersistencyOn && !isDebuggerMode(execution.getSystemContext())) return;
         // move all the events form the SystemContext into the event channel
         @SuppressWarnings("unchecked") ArrayDeque<ExecutionEvent> eventsQueue = (ArrayDeque) execution.getSystemContext().get(ExecutionConstants.EXECUTION_EVENTS_QUEUE);
         for (ExecutionEvent executionEvent : eventsQueue) {
