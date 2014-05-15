a8db5df11563bac67d6d8f26cc95bfefeb6f2901
oritstone <orit.stone@hp.com>
2014-05-15 14:25:00 +0300

    Refactor service to contain only Score methods

--- score-facade/src/main/java/com/hp/oo/orchestrator/services/configuration/WorkerConfigurationService.java
+++ score-facade/src/main/java/com/hp/oo/orchestrator/services/configuration/WorkerConfigurationService.java
@@ -1,38 +1,22 @@
 package com.hp.oo.orchestrator.services.configuration;
 
-import com.hp.oo.execution.debug.ExecutionInterrupt;
-
 import java.util.List;
-
-import static com.hp.oo.execution.debug.ExecutionInterrupt.InterruptType;
 
 /**
  * Created by IntelliJ IDEA.
  * User: kravtsov
  * Date: 03/06/12
  * Time: 14:35
+ * Used by Score for pause/cancel runs & stay in the worker
  */
 public interface WorkerConfigurationService {
 
-    public String getGroupByAlias(String aliasName, Long configurationVersion);
-
-    public String getSystemProperty(String name, Long configurationVersion) ;
-
-    public String getSelectionList(String name, Long configurationVersion) ;
-
-    public String getDomainTerm(String name, Long configurationVersion) ;
-
+    //todo: orit - refactor method to return boolean
     List<String> getCancelledExecutions();
 
     List<String> getWorkerGroups();
 
     boolean isExecutionPaused(String executionId, String branchId);
 
-    void refreshConfiguration();
-
-    void refreshWorkerGroups();
-
-	void enabled(boolean enabled);
-
-    ExecutionInterrupt getDebugInterrupt(String executionId, InterruptType interruptType, String key, String value);
+    void enabled(boolean enabled);
 }
