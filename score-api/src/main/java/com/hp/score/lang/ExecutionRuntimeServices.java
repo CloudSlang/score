d4add4b1d8ddf3d605f8918a0afc5b39393e101c
guymarom <marom.guy@gmail.com>
2014-06-17 10:39:59 +0300

    Added branch handling Added hashcode and equals methods

--- score-api/src/main/java/com/hp/score/lang/ExecutionRuntimeServices.java
+++ score-api/src/main/java/com/hp/score/lang/ExecutionRuntimeServices.java
@@ -1,10 +1,15 @@
 package com.hp.score.lang;
 
 import com.hp.score.api.ScoreEvent;
+import com.hp.score.vos.StartBranchDataContainer;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 import java.io.Serializable;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
@@ -17,12 +22,24 @@ public class ExecutionRuntimeServices implements Serializable {
     private static final long serialVersionUID = 2557429503280678353L;
 
     protected static final String EXECUTION_PAUSED = "EXECUTION_PAUSED";
+    private static final String BRANCH_DATA = "BRANCH_DATA";
 
     protected static final String SCORE_EVENTS_QUEUE = "SCORE_EVENTS_QUEUE";
 
     protected static final String NO_WORKERS_IN_GROUP = "NO_WORKERS_IN_GROUP";
 
     protected Map<String, Serializable> myMap = new HashMap<>();
+
+    public ExecutionRuntimeServices() {
+    }
+
+    public ExecutionRuntimeServices(Map<String, Serializable> parameters) {
+        this.myMap = new HashMap<>(parameters);
+    }
+
+    public ExecutionRuntimeServices(ExecutionRuntimeServices origin) {
+        this.myMap = new HashMap<>(origin.myMap);
+    }
 
     public void pause() {
         myMap.put(EXECUTION_PAUSED, Boolean.TRUE);
@@ -66,4 +83,45 @@ public class ExecutionRuntimeServices implements Serializable {
         return null;
     }
 
+    public void addBranch(Long startPosition, Long executionPlanId, Map<String, Serializable> context) {
+        addBranch(startPosition, executionPlanId, context, this);
+    }
+
+    protected void addBranch(Long startPosition, Long executionPlanId, Map<String, Serializable> context, ExecutionRuntimeServices executionRuntimeServices) {
+        if (!myMap.containsKey(BRANCH_DATA)) {
+            myMap.put(BRANCH_DATA, new ArrayList<StartBranchDataContainer>());
+        }
+        @SuppressWarnings("unchecked")
+        List<StartBranchDataContainer> branchesData = (List<StartBranchDataContainer>) myMap.get(BRANCH_DATA);
+        branchesData.add(new StartBranchDataContainer(startPosition, executionPlanId, context, executionRuntimeServices));
+    }
+
+    public List<StartBranchDataContainer> getBranchesData() {
+        @SuppressWarnings("unchecked")
+        List<StartBranchDataContainer> branchesData = (List<StartBranchDataContainer>) myMap.get(BRANCH_DATA);
+        return branchesData;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ExecutionRuntimeServices that = (ExecutionRuntimeServices) o;
+
+        return new EqualsBuilder()
+                .append(this.myMap, that.myMap)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+                .append(this.myMap)
+                .toHashCode();
+    }
 }
