d879297c6f9b2ba88a483c46ed24e368d7ac1adc
guymarom <marom.guy@gmail.com>
2014-07-07 19:09:11 +0300

    hello-score project - not working yet

---
+++ score-samples/hello-score/src/main/java/com/hp/score/samples/HelloScore.java
@@ -0,0 +1,35 @@
+package com.hp.score.samples;
+
+import com.hp.score.api.ExecutionPlan;
+import com.hp.score.api.ExecutionStep;
+import com.hp.score.api.Score;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * User: maromg
+ * Date: 22/06/2014
+ */
+public class HelloScore {
+
+    public static void main(String[] args) {
+        ApplicationContext context = loadScore();
+        ExecutionPlan executionPlan = createExecutionPlan();
+        Score score = context.getBean(Score.class);
+        score.trigger(executionPlan);
+    }
+
+    private static ApplicationContext loadScore() {
+        return new ClassPathXmlApplicationContext("/META-INF/spring/helloScoreContext.xml");
+    }
+
+    private static ExecutionPlan createExecutionPlan() {
+        ExecutionPlan executionPlan = new ExecutionPlan();
+
+        ExecutionStep executionStep = new ExecutionStep(0L);
+        executionPlan.addStep(executionStep);
+
+        return executionPlan;
+    }
+
+}
