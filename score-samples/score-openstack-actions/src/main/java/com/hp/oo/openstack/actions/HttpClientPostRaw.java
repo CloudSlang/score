c17e2bdc58cb8b05500bd0e3567e63bbc85bc029
Levente Bonczidai <levente.bonczidai@hp.com>
2014-07-15 17:03:18 +0300

    added http client samples

---
+++ score-samples/score-openstack-actions/src/main/java/com/hp/oo/openstack/actions/HttpClientPostRaw.java
@@ -0,0 +1,13 @@
+package com.hp.oo.openstack.actions;
+
+import org.apache.commons.httpclient.HttpClient;
+
+/**
+ * Created by bonczida on 7/15/2014.
+ */
+public class HttpClientPostRaw {
+    public void post(String userName, String password, String url) throws Exception {
+        HttpClient client = new HttpClient();
+    }
+
+}
