a1839778cad255371b09ab1238ffabd109b575cf
Natasha Beck <natasha.kravtsov@hp.com>
2014-07-21 10:30:35 +0300

    make db tests to work with liquibase

--- engine/node/oo-node-impl/src/test/java/com/hp/oo/engine/node/repositories/WorkerNodeRepositoryTest.java
+++ engine/node/oo-node-impl/src/test/java/com/hp/oo/engine/node/repositories/WorkerNodeRepositoryTest.java
@@ -1,141 +1,65 @@
 package com.hp.oo.engine.node.repositories;
 
 import com.hp.oo.engine.node.entities.WorkerNode;
-import com.hp.oo.engine.versioning.services.VersionService;
 import com.hp.oo.enginefacade.Worker;
-import com.hp.score.engine.data.SimpleHiloIdentifierGenerator;
 import junit.framework.Assert;
-import liquibase.integration.spring.SpringLiquibase;
-import org.hibernate.ejb.HibernatePersistence;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Properties;
-
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
-/**
- * Created by IntelliJ IDEA.
- * User: froelica
- * Date: 9/8/13
- * Time: 10:25 AM
- */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(classes = WorkerNodeRepositoryTest.Conf.class)
 @Transactional
 @TransactionConfiguration(defaultRollback = true)
 public class WorkerNodeRepositoryTest {
-	private static final boolean SHOW_SQL = false;
 
-	@Autowired
-	private WorkerNodeRepository workerNodeRepository;
+    @Autowired
+    private DataSource dataSource;
 
-	@Test
-	public void findGroupsTest() {
-		WorkerNode worker = new WorkerNode();
-		worker.setUuid("some faked uuid");
-		worker.setHostName("worker host name");
-		worker.setInstallPath("faked installation path");
-		worker.setPassword("faked password");
-		worker.setStatus(Worker.Status.RUNNING);
-		worker.setActive(true);
-		worker.setGroups(Arrays.asList("group1", "group2", "group3"));
-		workerNodeRepository.saveAndFlush(worker);
+    @Autowired
+   	private WorkerNodeRepository workerNodeRepository;
 
-		List<String> expected = Arrays.asList("group1", "group2");
-		List<String> result = workerNodeRepository.findGroups(expected);
-		Assert.assertEquals(expected, result);
-	}
+    protected JdbcTemplate jdbcTemplate;
 
-	@Configuration
-	@EnableJpaRepositories("com.hp.oo.engine.node.repositories")
-	@EnableTransactionManagement
-	static class Configurator {
-		@Bean
-		DataSource dataSource() {
-			return new EmbeddedDatabaseBuilder()
-					.setType(EmbeddedDatabaseType.H2)
-					.build();
-		}
+    @Before
+    public void init() {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
-		@Bean
-		SpringLiquibase liquibase(DataSource dataSource) {
-			SpringLiquibase liquibase = new SpringLiquibase();
-			liquibase.setDataSource(dataSource);
-			liquibase.setChangeLog("classpath:/META-INF/database/test.changes.xml");
-			SimpleHiloIdentifierGenerator.setDataSource(dataSource);
-			return liquibase;
-		}
+    @Test
+    public void findGroupsTest() {
+        WorkerNode worker = new WorkerNode();
+        worker.setUuid("some faked uuid");
+        worker.setHostName("worker host name");
+        worker.setInstallPath("faked installation path");
+        worker.setPassword("faked password");
+        worker.setStatus(Worker.Status.RUNNING);
+        worker.setActive(true);
+        worker.setGroups(Arrays.asList("group1", "group2", "group3"));
+        workerNodeRepository.saveAndFlush(worker);
 
+        List<String> expected = Arrays.asList("group1", "group2");
+        List<String> result = workerNodeRepository.findGroups(expected);
+        Assert.assertEquals(expected, result);
+    }
 
-		@Bean
-		Properties hibernateProperties() {
-			return new Properties(){{
-				setProperty("hibernate.format_sql", "true");
-				setProperty("hibernate.hbm2ddl.auto", "create-drop");
-				setProperty("hibernate.cache.use_query_cache", "false");
-				setProperty("hibernate.generate_statistics", "false");
-				setProperty("hibernate.cache.use_second_level_cache", "false");
-				setProperty("hibernate.order_updates", "true");
-				setProperty("hibernate.order_inserts", "true");
-			}};
-		}
+    @Configuration
+    @ImportResource({"classpath:/META-INF/spring/testContext.xml"})
+    @EnableJpaRepositories("com.hp.oo.engine.node.repositories")
+    static class Conf {
 
-		@Bean
-		JpaVendorAdapter jpaVendorAdapter() {
-			HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-			adapter.setShowSql(SHOW_SQL);
-			adapter.setGenerateDdl(true);
-			return adapter;
-		}
-
-		@Bean(name="entityManagerFactory")
-		@DependsOn("liquibase")
-		FactoryBean<EntityManagerFactory> emf(JpaVendorAdapter jpaVendorAdapter) {
-			LocalContainerEntityManagerFactoryBean fb = new LocalContainerEntityManagerFactoryBean();
-			fb.setJpaProperties(hibernateProperties());
-			fb.setDataSource(dataSource());
-			fb.setPersistenceProviderClass(HibernatePersistence.class);
-			fb.setPackagesToScan("com.hp.oo.engine.node");
-			fb.setJpaVendorAdapter(jpaVendorAdapter);
-			return fb;
-		}
-
-		@Bean
-		PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-			return new JpaTransactionManager(emf);
-		}
-
-		@Bean
-		public VersionService versionService() {
-			VersionService versionService = mock(VersionService.class);
-			when(versionService.getCurrentVersion(anyString())).thenReturn(1L);
-			return versionService;
-		}
-	}
+    }
 }
