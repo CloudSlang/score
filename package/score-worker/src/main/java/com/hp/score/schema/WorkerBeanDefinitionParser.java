a72b16a62fdb0994b3da788947eb1c042b6a21c3
Avi Moradi <avi.moradi@hp.com>
2014-09-04 13:32:10 +0300

    enable worker self registration in score US #6438

--- package/score-worker/src/main/java/com/hp/score/schema/WorkerBeanDefinitionParser.java
+++ package/score-worker/src/main/java/com/hp/score/schema/WorkerBeanDefinitionParser.java
@@ -1,8 +1,10 @@
 package com.hp.score.schema;
 
-import com.hp.score.worker.execution.reflection.ReflectionAdapterImpl;
-import com.hp.score.worker.execution.services.ExecutionServiceImpl;
-import com.hp.score.worker.execution.services.SessionDataServiceImpl;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractBeanDefinitionParser;
@@ -12,34 +14,41 @@ import org.springframework.util.StringUtils;
 import org.springframework.util.xml.DomUtils;
 import org.w3c.dom.Element;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.hp.score.events.EventBusImpl;
+import com.hp.score.worker.execution.reflection.ReflectionAdapterImpl;
+import com.hp.score.worker.execution.services.ExecutionServiceImpl;
+import com.hp.score.worker.execution.services.SessionDataServiceImpl;
+import com.hp.score.worker.management.WorkerConfigurationServiceImpl;
+import com.hp.score.worker.management.WorkerRegistration;
+import com.hp.score.worker.management.services.InBuffer;
+import com.hp.score.worker.management.services.OutboundBufferImpl;
+import com.hp.score.worker.management.services.RetryTemplate;
+import com.hp.score.worker.management.services.SimpleExecutionRunnableFactory;
+import com.hp.score.worker.management.services.SynchronizationManagerImpl;
+import com.hp.score.worker.management.services.WorkerManager;
+import com.hp.score.worker.management.services.WorkerManagerMBean;
+import com.hp.score.worker.management.services.WorkerRecoveryManagerImpl;
 
 /**
- * Date: 1/21/14
- *
  * @author Dima Rassin
+ * @since 21/01/2014
  */
-@SuppressWarnings("unused")
 public class WorkerBeanDefinitionParser extends AbstractBeanDefinitionParser {
 
 	private Map<Class<?>,String> beans = new HashMap<Class<?>,String>(){{
-		put(com.hp.score.worker.management.services.WorkerManager.class, "workerManager");
-		put(com.hp.score.events.EventBusImpl.class, null);
+		put(WorkerManager.class, "workerManager");
+		put(EventBusImpl.class, null);
 		put(ExecutionServiceImpl.class, "agent");
-		put(com.hp.score.worker.management.services.InBuffer.class, null);
-		put(com.hp.score.worker.management.services.OutboundBufferImpl.class, "outBuffer");
-		put(com.hp.score.worker.management.services.RetryTemplate.class, null);
-		put(com.hp.score.worker.management.services.SimpleExecutionRunnableFactory.class, null);
-		put(com.hp.score.worker.management.services.WorkerManagerMBean.class, "com.hp.oo.execution.services.WorkerManagerMBean");
-		put(com.hp.score.worker.management.services.WorkerRecoveryManagerImpl.class, null);
+		put(InBuffer.class, null);
+		put(OutboundBufferImpl.class, "outBuffer");
+		put(RetryTemplate.class, null);
+		put(SimpleExecutionRunnableFactory.class, null);
+		put(WorkerManagerMBean.class, "com.hp.oo.execution.services.WorkerManagerMBean");
+		put(WorkerRecoveryManagerImpl.class, null);
 		put(ReflectionAdapterImpl.class, null);
         put(SessionDataServiceImpl.class, "sessionDataService");
-		put(com.hp.score.worker.management.services.SynchronizationManagerImpl.class, null);
-		put(com.hp.score.worker.management.WorkerConfigurationServiceImpl.class, "workerConfiguration");
-//		put(org.score.worker.management.WorkerRegistration.class, null);
+		put(SynchronizationManagerImpl.class, null);
+		put(WorkerConfigurationServiceImpl.class, "workerConfiguration");
 	}};
 
 	private List<ConfValue> configurationValues = Arrays.asList(
@@ -58,15 +67,11 @@ public class WorkerBeanDefinitionParser extends AbstractBeanDefinitionParser {
 	@Override
 	protected AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
 		registerWorkerUuid(element.getAttribute("uuid"), element.getAttribute("depends-on"), parserContext);
-
 		registerBeans(parserContext);
-
+		registerSpecialBeans(element, parserContext);
 		registerConfiguration(DomUtils.getChildElementByTagName(element, "configuration"), parserContext);
-
 		registerSpringIntegration(parserContext);
-
 		registerScheduler(DomUtils.getChildElementByTagName(element, "scheduler"), parserContext);
-
 		return createRootBeanDefinition();
 	}
 
@@ -91,6 +96,12 @@ public class WorkerBeanDefinitionParser extends AbstractBeanDefinitionParser {
 					.NAME(entry.getValue())
 					.CLASS(entry.getKey())
 					.register();
+		}
+	}
+
+	private static void registerSpecialBeans(Element element, ParserContext parserContext) {
+		if(!"false".equalsIgnoreCase(element.getAttribute("register"))) {
+			new BeanRegistrator(parserContext).CLASS(WorkerRegistration.class).register();
 		}
 	}
 
