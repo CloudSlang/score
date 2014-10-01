bbf9bb10e73e41781aed40438186f057513a631e
meshipeer <meshi.peer@hp.com>
2014-10-01 12:59:46 +0300

    add JavaDocs

--- engine/score-engine-jobs/src/main/java/com/hp/score/job/ScoreEngineJobs.java
+++ engine/score-engine-jobs/src/main/java/com/hp/score/job/ScoreEngineJobs.java
@@ -6,17 +6,31 @@ package com.hp.score.job;
  * Time: 10:35
  */
 //TODO: Add Javadoc
-//TODO: Do we want to move to api module?
 public interface ScoreEngineJobs {
 
+    /**
+     * job that clean the finished steps from the queue
+     */
     void cleanQueueJob();
 
+    /**
+     * job that join all the suspended execution of brunches that finished
+     */
     void joinFinishedSplitsJob();
 
+    /**
+     * job that create rolling in the partition table
+     */
     void statesRollingJob();
 
+    /**
+     * job that update version number - we use it instead of time
+     */
     void recoveryVersionJob();
 
+    /**
+     *  job that recover workers that didn't send keep alive
+     */
     void executionRecoveryJob();
 
 }
