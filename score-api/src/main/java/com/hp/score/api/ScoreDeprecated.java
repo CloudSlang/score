a2755ceb8d3e24cb76c89135e1baf4597cb47b75
meshipeer <meshi.peer@hp.com>
2014-10-01 08:04:30 +0300

    add JavaDocs

--- score-api/src/main/java/com/hp/score/api/ScoreDeprecated.java
+++ score-api/src/main/java/com/hp/score/api/ScoreDeprecated.java
@@ -3,11 +3,22 @@ package com.hp.score.api;
 /**
  * Created by peerme on 23/07/2014.
  */
-//TODO: Add Javadoc
 @Deprecated
 public interface ScoreDeprecated {
 
+    /***
+     * for cases you need the executionId before triggering
+     * this method generate executionId
+     * @return  the executionId generated
+     */
     public Long generateExecutionId();
 
+    /**
+     * for cases you need the executionId before triggering
+     * trigger run with pre-generated executionId (by using generateExecutionId() method...)
+     * @param executionId  - the executionId for the run
+     * @param triggeringProperties   object holding all the properties needed for the trigger
+     * @return the give executionId
+     */
     public Long trigger(Long executionId, TriggeringProperties triggeringProperties);
 }
