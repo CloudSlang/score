5002603c4be753f477c7e43ad5857447361c97d8
Meir Wahnon <meir.wahnon@hp.com>
2014-11-05 15:17:14 +0200

    modify Input class for Slang

--- score-language/score-lang-compiler/src/main/java/com/hp/score/lang/compiler/domain/Input.java
+++ score-language/score-lang-compiler/src/main/java/com/hp/score/lang/compiler/domain/Input.java
@@ -3,18 +3,49 @@ package com.hp.score.lang.compiler.domain;
 public class Input {
 
     private final String name;
-    private final String value;
 
-    public Input(String name, String value) {
+    private final String defaultValue;
+
+    private final String expression ;
+
+    private final boolean encrypted;
+
+    private final boolean required;
+
+    public Input(String name) {
         this.name = name;
-        this.value = value;
+        this.defaultValue = null;
+        this.expression = null;
+        this.encrypted = false ;
+        this.required = true ;
+    }
+
+    public Input(String name, String defaultValue,String expression,boolean encrypted,boolean required) {
+        this.name = name;
+        this.defaultValue = defaultValue;
+        this.expression = expression;
+        this.encrypted = encrypted ;
+        this.required = required ;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getValue() {
-        return value;
+    public String getDefaultValue() {
+        return defaultValue;
     }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public boolean isEncrypted() {
+        return encrypted;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
 }
--- score-language/score-lang-compiler/src/main/java/com/hp/score/lang/compiler/transformers/InputsTransformer.java
+++ score-language/score-lang-compiler/src/main/java/com/hp/score/lang/compiler/transformers/InputsTransformer.java
@@ -5,6 +5,7 @@ import com.hp.score.lang.compiler.Transformer;
 import com.hp.score.lang.compiler.domain.Input;
 import org.springframework.stereotype.Component;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,6 +15,14 @@ import java.util.Map;
 @Component
 public class InputsTransformer implements Transformer<List<Object>, List<Input>> {
 
+    private static final String DEFAULT_KEY = "default";
+
+    private static final String EXPRESSION_KEY = "expression";
+
+    private static final String REQUIRED_KEY = "required";
+
+    private static final String ENCRYPTED_KEY = "encrypted";
+
     @Override
     public List<Input> transform(List<Object> rawData) {
         List<Input> result = new ArrayList<>();
@@ -21,30 +30,46 @@ public class InputsTransformer implements Transformer<List<Object>, List<Input>>
             //- some_input
             //this is our default behavior that if the user specifies only a key, the key is also the ref we look for
             if (rawInput instanceof String) {
-                result.add(new Input((String) rawInput, (String) rawInput));
+                result.add(createRefInput((String)rawInput));
             } else if (rawInput instanceof Map) {
                 Map.Entry entry = (Map.Entry) ((Map) rawInput).entrySet().iterator().next();
                 // - some_input: some_expression
                 // the value of the input is an expression we need to evaluate at runtime
                 if (entry.getValue() instanceof String){
-                    result.add(new Input((String) entry.getKey(), (String) entry.getValue()));
+                    result.add(createExpressionInput(entry));
                 }
                 // - some_inputs:
                 //      property1: value1
                 //      property2: value2
                 // this is the verbose way of defining inputs with all of the properties available
                 else if (entry.getValue() instanceof Map) {
-                    result.add(new Input((String) entry.getKey(), (String) entry.getKey()));
+                    result.add(createPropInput(entry));
                 }
             }
         }
         return result;
     }
 
+    private Input createPropInput(Map.Entry<String,Map<String,Serializable>> entry) {
+        Map<String,Serializable> prop = entry.getValue();
+        boolean required = prop.containsKey(REQUIRED_KEY) && ((boolean)prop.get(REQUIRED_KEY));
+        boolean encrypted = prop.containsKey(ENCRYPTED_KEY) && ((boolean)prop.get(ENCRYPTED_KEY));
+        String expression = prop.containsValue(EXPRESSION_KEY) ? ((String)prop.get(ENCRYPTED_KEY)) : null;
+        String defaultValue = prop.containsValue(DEFAULT_KEY) ? ((String)prop.get(DEFAULT_KEY)) : null;
+        return new Input(entry.getKey(),defaultValue,expression,encrypted,required);
+    }
+
+    private Input createExpressionInput(Map.Entry<String, String> entry) {
+        return new Input(entry.getKey(),null,entry.getValue(),false,true);
+    }
+
+    private Input createRefInput(String rawInput) {
+       return new Input(rawInput);//make it use the exp
+    }
+
     @Override
     public List<Scope> getScopes() {
         return Arrays.asList(Scope.BEFORE_OPERATION);
     }
-
 
 }
