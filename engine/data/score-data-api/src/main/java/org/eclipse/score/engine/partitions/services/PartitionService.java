7bfe61ca487bb5c1c3d6b306a0cb54188477bd36
Eliya Sadan <eliyasadan@gmail.com>
2014-12-11 12:01:30 +0200

    PartitionService docs

--- engine/data/score-data-api/src/main/java/org/eclipse/score/engine/partitions/services/PartitionService.java
+++ engine/data/score-data-api/src/main/java/org/eclipse/score/engine/partitions/services/PartitionService.java
@@ -19,11 +19,48 @@ import org.eclipse.score.engine.partitions.entities.PartitionGroup;
  * Date: 4/22/12
  *
  * @author
+ *
+ * This service is responsible for handling tables partiotions.
+ * Partition tables are rolled to the next partition when passing a certian treshold
+ *
  */
-//TODO: Add Javadoc
 public interface PartitionService {
+
+    /**
+     * creates a partition group and store it to the DB
+     *
+     * @param groupName the desired name of the group
+     * @param groupSize the size of partitions
+     * @param timeThreshold a time threshold to roll to the next partition
+     * @param sizeThreshold a size threshold to roll to the next partition
+     */
     void createPartitionGroup(String groupName, int groupSize, long timeThreshold, long sizeThreshold);
+
+    /**
+     * updates a partition group and store it to the DB
+     *
+     * @param groupName the desired name of the group
+     * @param groupSize the size of partitions
+     * @param timeThreshold a time threshold to roll to the next partition
+     * @param sizeThreshold a size threshold to roll to the next partition
+     */
     void updatePartitionGroup(String groupName, int groupSize, long timeThreshold, long sizeThreshold);
+
+    /**
+     *
+     * Returns a group of the given name.
+     *
+     * @param groupName the group name to read
+     * @return a {@link org.eclipse.score.engine.partitions.entities.PartitionGroup} of the given name
+     */
 	PartitionGroup readPartitionGroup(String groupName);
+
+    /**
+     *
+     * Rolls a partition 
+     *
+     * @param groupName the group name to roll
+     * @return true if roll was successful
+     */
 	boolean rollPartitions(String groupName);
 }
