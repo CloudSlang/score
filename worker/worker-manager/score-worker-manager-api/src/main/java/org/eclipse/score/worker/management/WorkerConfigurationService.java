8320af6d30b5cce284023ca656cf0ea6b171a341
Eliya Sadan <eliyasadan@gmail.com>
2014-12-11 11:51:06 +0200

    worker configuration service docs

--- worker/worker-manager/score-worker-manager-api/src/main/java/org/eclipse/score/worker/management/WorkerConfigurationService.java
+++ worker/worker-manager/score-worker-manager-api/src/main/java/org/eclipse/score/worker/management/WorkerConfigurationService.java
@@ -17,18 +17,41 @@ package org.eclipse.score.worker.management;
  * @author kravtsov
  * @author Avi Moradi
  * @since 03/06/2012
- * @version $Id$
  * Used by Score for pause/cancel runs & stay in the worker
  */
-//TODO: Add Javadoc
 public interface WorkerConfigurationService {
 
+    /**
+     * checks if the given execution is pending cancel
+     *
+     * @param executionId the execution id to check
+     * @return true if the execution is pending cancel
+     */
 	public boolean isExecutionCancelled(Long executionId);
 
+    /**
+     * checks if the given execution is pending pause
+     *
+     * @param executionId the execution id to check
+     * @return true if the execution is pending pause
+     */
 	public boolean isExecutionPaused(Long executionId, String branchId);
 
+    /**
+     *
+     * checks if the current worker is part of the fiven group
+     *
+     * @param group the group to check
+     * @return true if the worker is part of the group
+     */
 	public boolean isMemberOf(String group);
 
+    /**
+     * Sets the current worker enabled state
+     * relevant for tasks not to work until the worker is enabled
+     *
+     * @param enabled the edibility state to set
+     */
 	public void setEnabled(boolean enabled);
 
 }
