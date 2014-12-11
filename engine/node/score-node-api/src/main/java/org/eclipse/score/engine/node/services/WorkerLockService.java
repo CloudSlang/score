689d66516560fc5acb1f39ac34fe799c5be5ced8
Eliya Sadan <eliyasadan@gmail.com>
2014-12-11 11:21:29 +0200

    WorkerLockService docs should be added by Natasha

--- engine/node/score-node-api/src/main/java/org/eclipse/score/engine/node/services/WorkerLockService.java
+++ engine/node/score-node-api/src/main/java/org/eclipse/score/engine/node/services/WorkerLockService.java
@@ -18,7 +18,7 @@ package org.eclipse.score.engine.node.services;
  * Date: 20/07/14
  * Time: 11:25
  */
-//TODO: Add Javadoc Eliya
+//TODO: Add Javadoc Natasha
 public interface WorkerLockService {
 
     void create(String uuid);
