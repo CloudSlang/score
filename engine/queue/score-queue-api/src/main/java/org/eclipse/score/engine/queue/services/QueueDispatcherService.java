515555e066f0661e18c0cd3cbdb1eb6f90faff9c
Eliya Sadan <eliyasadan@gmail.com>
2014-12-11 16:36:06 +0200

    QueueDispatcherService.java docs

--- engine/queue/score-queue-api/src/main/java/org/eclipse/score/engine/queue/services/QueueDispatcherService.java
+++ engine/queue/score-queue-api/src/main/java/org/eclipse/score/engine/queue/services/QueueDispatcherService.java
@@ -24,13 +24,37 @@ import java.util.List;
  * User:
  * Date: 10/09/12
  * Time: 11:00
+ *
+ * handles dispatching adn polling messages from the queue
+ *
  */
-//TODO: Add Javadoc
 public interface QueueDispatcherService {
 
+    /**
+     *
+     * dispatch messages to the queue
+     *
+     * @param messages the messages to dispatch
+     */
 	void dispatch(List<ExecutionMessage> messages);
 
+    /**
+     *
+     * @param uuid the worker id
+     * @param maxSize max size of the poll bulk
+     * @param createDate the first message create date
+     * @return a list of {@link org.eclipse.score.engine.queue.entities.ExecutionMessage}
+     */
 	List<ExecutionMessage> poll(String uuid, int maxSize, Date createDate);
 
+    /**
+     *
+     * Dispatch one message to the queue
+     *
+     * @param messageId the id of the message
+     * @param group the group associated with the message
+     * @param status the message status
+     * @param payload the message payload
+     */
 	void dispatch(String messageId, String group, ExecStatus status, Payload payload);
 }
