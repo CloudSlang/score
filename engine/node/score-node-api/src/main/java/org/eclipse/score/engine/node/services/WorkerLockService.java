abd38381b51dc247463480980d28758bb25d698b
Evgeny Varela Savela <evgeny.varela@hp.com>
2014-12-15 18:48:55 +0200

    add javadoc

--- engine/node/score-node-api/src/main/java/org/eclipse/score/engine/node/services/WorkerLockService.java
+++ engine/node/score-node-api/src/main/java/org/eclipse/score/engine/node/services/WorkerLockService.java
@@ -17,13 +17,27 @@ package org.eclipse.score.engine.node.services;
  * User: varelasa
  * Date: 20/07/14
  * Time: 11:25
+ *
+ * A service responsible for synchronizing between drain and recovery mechanisms
  */
-//TODO: Add Javadoc Natasha
+
 public interface WorkerLockService {
 
+    /**
+     * Create the Worker Lock entry with the current worker uuid
+     * @param uuid worker's unique identifier
+     */
     void create(String uuid);
 
+    /**
+     * Delete the Worker Lock entry with the current worker uuid
+     * @param uuid worker's unique identifier
+     */
     void delete(String uuid);
 
+    /**
+     * Lock the Worker Lock entity with the current worker uuid
+     * @param uuid worker's unique identifier
+     */
     void lock(String uuid);
 }
