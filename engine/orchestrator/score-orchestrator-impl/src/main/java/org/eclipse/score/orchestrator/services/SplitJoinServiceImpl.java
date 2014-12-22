fb048476108b16baf840e3f1ac3667d95a0d7f9a
Meir Wahnon <meir.wahnon@hp.com>
2014-12-22 15:26:38 +0200

    change license header for xml+ java files (remove eclipse part)

--- engine/orchestrator/score-orchestrator-impl/src/main/java/org/eclipse/score/orchestrator/services/SplitJoinServiceImpl.java
+++ engine/orchestrator/score-orchestrator-impl/src/main/java/org/eclipse/score/orchestrator/services/SplitJoinServiceImpl.java
@@ -1,16 +1,13 @@
 /*******************************************************************************
- * (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Apache License v2.0 which accompany this distribution.
- *
- * The Eclipse Public License is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * The Apache License is available at
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- *******************************************************************************/
+* (c) Copyright 2014 Hewlett-Packard Development Company, L.P.
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Apache License v2.0 which accompany this distribution.
+*
+* The Apache License is available at
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+*******************************************************************************/
+
 package org.eclipse.score.orchestrator.services;
 
 import ch.lambdaj.function.convert.Converter;
