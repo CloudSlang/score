61817f79532978608704f090bbe0774c50ae88b5
SamMarkowitz <sammie.markowitz@gmail.com>
2015-02-02 14:44:44 +0200

    fixed typo and readability
    Signed-off-by: Sam Markowitz <sam.markowitz@hp.com>

--- score-api/src/main/java/org/openscore/events/EventBus.java
+++ score-api/src/main/java/org/openscore/events/EventBus.java
@@ -20,20 +20,20 @@ import java.util.Set;
 public interface EventBus {
 
     /**
-     *  register listener to event types
+     * register listener for event types
      * @param eventHandler  - the handler of the events
      * @param eventTypes - the types of events you want to listen to
      */
 	void subscribe(ScoreEventListener eventHandler, Set<String> eventTypes);
 
     /**
-     * removw the given handler
+     * remove the given handler
      * @param eventHandler - the listener to remove
      */
 	void unsubscribe(ScoreEventListener eventHandler);
 
     /**
-     * dispatch the given events, means according to their types relevant handlers will be called
+     * dispatch the given events, meaning relevant handlers will be called based on the event types
      * @param eventWrappers one or more score event to dispatch
      */
 	void dispatch(ScoreEvent... eventWrappers) throws InterruptedException;
