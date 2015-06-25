4edecfa25e31cd1b57513995c23521a13b3804f3
unknown <varelase@VARELASE1.emea.hpqcorp.net>
2015-06-25 17:04:39 +0300

    Change metadata during changing running execution plan

--- score-api/src/main/java/io/cloudslang/score/api/execution/ExecutionMetadataConsts.java
+++ score-api/src/main/java/io/cloudslang/score/api/execution/ExecutionMetadataConsts.java
@@ -7,5 +7,5 @@ public class ExecutionMetadataConsts {
 
     public static final String EXECUTION_PLAN_ID = "EXECUTION_PLAN_ID";
     public static final String EXECUTION_PLAN_NAME = "EXECUTION_PLAN_NAME";
-    public static final String RUNNING_USER = "RUNNING_USER";
+
 }
