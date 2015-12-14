659f8258c5e0be937965b2448f38569ecb7a7d13
Natasha Beck <natasha.kravtsov@hpe.com>
2015-12-14 11:51:22 +0200

    Remove @Service - was added by mistake

--- worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/WorkerVersionServiceImpl.java
+++ worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/WorkerVersionServiceImpl.java
@@ -1,12 +1,9 @@
 package io.cloudslang.worker.management.services;
 
-import org.springframework.stereotype.Service;
-
 /**
  * Created by kravtsov on 07/12/2015
  */
 
-@Service
 public class WorkerVersionServiceImpl implements WorkerVersionService {
     @Override
     public String getWorkerVersion() {
