f51db65bc56c70580a8f11faa9eaca26d83f3f9e
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-02-11 11:56:32 +0200

    Remove library
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- engine/orchestrator/score-orchestrator-impl/src/main/java/io/cloudslang/orchestrator/services/OrchestratorDispatcherServiceImpl.java
+++ engine/orchestrator/score-orchestrator-impl/src/main/java/io/cloudslang/orchestrator/services/OrchestratorDispatcherServiceImpl.java
@@ -23,28 +23,26 @@ import io.cloudslang.engine.queue.services.QueueDispatcherService;
 import io.cloudslang.orchestrator.entities.SplitMessage;
 import org.apache.commons.lang.Validate;
 import org.apache.log4j.Logger;
-import org.hamcrest.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import static ch.lambdaj.Lambda.filter;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Date: 12/1/13
- *
  */
 public final class OrchestratorDispatcherServiceImpl implements OrchestratorDispatcherService {
-	private final Logger logger = Logger.getLogger(getClass());
 
-	@Autowired
-	private QueueDispatcherService queueDispatcher;
+    private final Logger logger = Logger.getLogger(getClass());
 
-	@Autowired
-	private SplitJoinService splitJoinService;
+    @Autowired
+    private QueueDispatcherService queueDispatcher;
+
+    @Autowired
+    private SplitJoinService splitJoinService;
 
     @Autowired
     private WorkerNodeService workerNodeService;
@@ -64,14 +62,15 @@ public final class OrchestratorDispatcherServiceImpl implements OrchestratorDisp
         String currentWRV = workerNodeService.readByUUID(workerUuid).getWorkerRecoveryVersion();
 
         //This is done in order to make sure that if we do retries in worker we won't insert same bulk twice
-        if(currentBulkNumber!=null && currentBulkNumber.equals(bulkNumber)){
-            logger.warn("Orchestrator got messages bulk with same bulk number: " + bulkNumber + " This bulk was inserted to DB before. Discarding...");
+        if (currentBulkNumber != null && currentBulkNumber.equals(bulkNumber)) {
+            logger.warn("Orchestrator got messages bulk with same bulk number: " + bulkNumber
+                    + " This bulk was inserted to DB before. Discarding...");
         }
         //This is done in order to make sure that we are not getting messages from worker that was already recovered and does not know about it yet
-        else if(!currentWRV.equals(wrv)){
-            logger.warn("Orchestrator got messages from worker: " + workerUuid + " with wrong WRV:" + wrv + " Current WRV is: " + currentWRV +  ". Discarding...");
-        }
-        else {
+        else if (!currentWRV.equals(wrv)) {
+            logger.warn("Orchestrator got messages from worker: " + workerUuid + " with wrong WRV:" + wrv
+                    + " Current WRV is: " + currentWRV + ". Discarding...");
+        } else {
             dispatch(messages);
             workerNodeService.updateBulkNumber(workerUuid, bulkNumber);
         }
@@ -80,42 +79,36 @@ public final class OrchestratorDispatcherServiceImpl implements OrchestratorDisp
     private void dispatch(List<? extends Serializable> messages) {
         Validate.notNull(messages, "Messages list is null");
 
-        if (logger.isDebugEnabled()) logger.debug("Dispatching " + messages.size() + " messages");
+        if (logger.isDebugEnabled()) {
+            logger.debug("Dispatching " + messages.size() + " messages");
+        }
         long t = System.currentTimeMillis();
-        final AtomicInteger messagesCounter = new AtomicInteger(0);
 
-        dispatch(messages, ExecutionMessage.class, new Handler<ExecutionMessage>() {
-            @Override
-            public void handle(List<ExecutionMessage> messages) {
-                messagesCounter.addAndGet(messages.size());
-                queueDispatcher.dispatch(messages);
-            }
-        });
+        queueDispatcher.dispatch(messages.stream()
+                .filter(ExecutionMessage.class::isInstance)
+                .map(ExecutionMessage.class::cast)
+                .collect(toList())
+        );
 
-        dispatch(messages, SplitMessage.class, new Handler<SplitMessage>() {
-            @Override
-            public void handle(List<SplitMessage> messages) {
-                messagesCounter.addAndGet(messages.size());
-                splitJoinService.split(messages);
-            }
-        });
+        splitJoinService.split(messages.stream()
+                .filter(SplitMessage.class::isInstance)
+                .map(SplitMessage.class::cast)
+                .collect(toList())
+        );
 
-        t = System.currentTimeMillis()-t;
-        if (logger.isDebugEnabled()) logger.debug("Dispatching " + messagesCounter.get() + " messages is done in " + t + " ms");
-        if (messages.size() > messagesCounter.get()){
-            logger.warn((messages.size() - messagesCounter.get()) + " messages were not being dispatched, since unknown type");
+        int count = (int) messages.stream()
+                .filter(message -> !(message instanceof SplitMessage) && !(message instanceof ExecutionMessage))
+                .count();
+
+        int dispatched = messages.size() - count;
+
+        t = System.currentTimeMillis() - t;
+        if (logger.isDebugEnabled()) {
+            logger.debug("Dispatching " + dispatched + " messages is done in " + t + " ms");
+        }
+        if (count > 0) {
+            logger.warn(count + " messages were not being dispatched, since unknown type");
         }
     }
 
-	private <T extends Serializable> void dispatch(List<? extends Serializable> messages, Class<T> messageClass, Handler<T> handler){
-		@SuppressWarnings("unchecked")
-		List<T> filteredMessages = (List<T>) filter(Matchers.instanceOf(messageClass), messages);
-		if (!messages.isEmpty()){
-			handler.handle(filteredMessages);
-		}
-	}
-
-	private interface Handler<T>{
-		public void handle(List<T> messages);
-	}
 }
