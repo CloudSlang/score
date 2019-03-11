2eedb18aca48379e238e3a38b71b00c03e483bf0
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-03-11 17:47:25 +0200

    Gzip ExecutionMessage
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- engine/queue/score-queue-api/src/main/java/io/cloudslang/engine/queue/entities/ExecutionMessageConverter.java
+++ engine/queue/score-queue-api/src/main/java/io/cloudslang/engine/queue/entities/ExecutionMessageConverter.java
@@ -16,124 +16,181 @@
 
 package io.cloudslang.engine.queue.entities;
 
-import org.apache.commons.io.IOUtils;
 import io.cloudslang.score.facade.entities.Execution;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
-/**
- * Created with IntelliJ IDEA.
- * User: kravtsov
- * Date: 20/11/12
- * Time: 14:35
- */
+
 public class ExecutionMessageConverter {
 
-	public static final int SIZE = 1024;
-	@Autowired(required = false)
-	private SensitiveDataHandler sensitiveDataHandler;
+    private static final byte[] ENCRYPTION_MARKER = new byte[]{73, 28, 45, 109};
+    private static final int SIZE = 1024;
 
-	public <T> T extractExecution(Payload payload) {
-		return objFromBytes(payload.getData());
-	}
+    @Autowired(required = false)
+    private SensitiveDataHandler sensitiveDataHandler;
 
-	public Payload createPayload(Execution execution) {
-		return createPayload(execution, false);
-	}
+    public <T> T extractExecution(Payload payload) {
+        return objFromBytes(payload.getData());
+    }
 
-	public Payload createPayload(Execution execution, boolean setContainsSensitiveData) {
-		Payload payload = new Payload(objToBytes(execution));
-		if(setContainsSensitiveData || checkContainsSensitiveData(execution)) {
-			setSensitive(payload);
-		}
-		return payload;
-	}
+    public Payload createPayload(Execution execution) {
+        return createPayload(execution, false);
+    }
 
-	private boolean checkContainsSensitiveData(Execution execution) {
-		return sensitiveDataHandler != null &&
-				sensitiveDataHandler.containsSensitiveData(execution.getSystemContext(), execution.getContexts());
-	}
+    public Payload createPayload(Execution execution, boolean setContainsSensitiveData) {
+        Payload payload = new Payload(objToBytes(execution));
+        if (setContainsSensitiveData || checkContainsSensitiveData(execution)) {
+            setSensitive(payload);
+        }
+        return payload;
+    }
 
-	public boolean containsSensitiveData(Payload payload) {
-		return isSensitive(payload);
-	}
+    private boolean checkContainsSensitiveData(Execution execution) {
+        return sensitiveDataHandler != null &&
+                sensitiveDataHandler.containsSensitiveData(execution.getSystemContext(), execution.getContexts());
+    }
 
-	private <T> T objFromBytes(byte[] bytes) {
-		ObjectInputStream ois = null;
-		try {
-			//2 Buffers are added to increase performance
-			ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+    public boolean containsSensitiveData(Payload payload) {
+        return isSensitive(payload);
+    }
 
-			skipPayloadMetaData(is);
+    private boolean areBytesCompressed(byte[] bytes) {
+        // sensitive is 1 byte, Encryption marker is 4 bytes so compressed is at least 5 in length
+        if (bytes.length < 5) {
+            return false;
+        }
 
-			BufferedInputStream bis = new BufferedInputStream(is);
-			ois = new ObjectInputStream(bis);
+        // We considered encrypted if ENCRYPTION_MARKER starts in bytes from index 1.
+        for (int i = 0; i < ENCRYPTION_MARKER.length; i++) {
+            if (ENCRYPTION_MARKER[i] != bytes[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-			//noinspection unchecked
-			return (T)ois.readObject();
-		}
-		catch(IOException | ClassNotFoundException ex) {
-			throw new RuntimeException("Failed to read execution plan from byte[]. Error: ", ex);
-		}
-		finally {
-			IOUtils.closeQuietly(ois);
-		}
+    private <T> T objFromBytes(byte[] bytes) {
+        ObjectInputStream ois = null;
+        try {
+            if (areBytesCompressed(bytes)) { // Compressed object
+                ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+                skipPayloadMetaData(is);
+                skipEncryptionMarker(is);
 
-	}
+                byte[] uncompressedBytes = gzipUncompress(is);
+                ois = new ObjectInputStream(new ByteArrayInputStream(uncompressedBytes));
 
-	private byte[] objToBytes(Object obj){
-		ObjectOutputStream oos = null;
-		try {
-			ByteArrayOutputStream bout = new ByteArrayOutputStream(SIZE);
+                // noinspection unchecked
+                return (T) ois.readObject();
+            } else {
+                // 2 buffers are added to increase performance
+                ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+                skipPayloadMetaData(is);
 
-			initPayloadMetaData(bout);
+                BufferedInputStream bis = new BufferedInputStream(is);
+                ois = new ObjectInputStream(bis);
 
-			BufferedOutputStream bos = new BufferedOutputStream(bout);
-			oos = new ObjectOutputStream(bos);
+                // noinspection unchecked
+                return (T) ois.readObject();
+            }
+        } catch (IOException | ClassNotFoundException ex) {
+            throw new RuntimeException("Failed to read execution plan from byte[]. Error: ", ex);
+        } finally {
+            IOUtils.closeQuietly(ois);
+        }
+    }
 
-			oos.writeObject(obj);
-			oos.flush();
+    private byte[] objToBytes(Object obj) {
+        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(SIZE)) {
+            initPayloadMetaData(byteArrayOutputStream);
+            addEncryptionMarker(byteArrayOutputStream);
+            byteArrayOutputStream.write(getCompressedObjectBytes(obj));
 
-			return bout.toByteArray();
-		}
-		catch(IOException ex) {
-			throw new RuntimeException("Failed to serialize execution plan. Error: ", ex);
-		} finally {
-			IOUtils.closeQuietly(oos);
-		}
-	}
+            return byteArrayOutputStream.toByteArray();
+        } catch (IOException ex) {
+            throw new RuntimeException("Failed to serialize execution plan. Error: ", ex);
+        }
+    }
 
-	/***************************************************************************************/
-	//we padding payload with clean bytes which then will be used for metadata writing
-	private static final byte[] PAYLOAD_META_DATA_INIT_BYTES = {0};
+    private byte[] getCompressedObjectBytes(Object obj) throws IOException {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(SIZE);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(baos)) {
+            objectOutputStream.writeObject(obj);
+            objectOutputStream.flush();
 
-	//for now meta data is only one byte
-	private static final int INFRA_PART_BYTE = 0;
+            return gzipCompress(baos.toByteArray());
+        }
+    }
 
-	private static final int IS_SENSITIVE = 1;
+    private void addEncryptionMarker(ByteArrayOutputStream bout) {
+        bout.write(ENCRYPTION_MARKER, 0, ENCRYPTION_MARKER.length);
+    }
 
-	private void setSensitive(Payload payload) {
-		payload.getData()[INFRA_PART_BYTE] = IS_SENSITIVE;
-	}
+    private static byte[] gzipCompress(byte[] uncompressedBytes) {
+        // In order to call close() from try with resources
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(uncompressedBytes.length);
+        try (GZIPOutputStream gzipOs = new GZIPOutputStream(baos)) {
+            gzipOs.write(uncompressedBytes);
+        } catch (IOException gzipExc) {
+            throw new RuntimeException("Failed to compress byte array: ", gzipExc);
+        }
+        // In order to call close() from try with resources
+        return baos.toByteArray();
+    }
 
-	private boolean isSensitive(Payload payload) {
-		return payload.getData()[INFRA_PART_BYTE] == IS_SENSITIVE;
-	}
+    private static byte[] gzipUncompress(ByteArrayInputStream bis) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream(); GZIPInputStream gzipIs = new GZIPInputStream(bis)) {
+            byte[] buffer = new byte[SIZE];
+            int len;
+            while ((len = gzipIs.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            return baos.toByteArray();
+        } catch (IOException gzipExc) {
+            throw new RuntimeException("Failed to uncompress byte array: ", gzipExc);
+        }
+    }
 
-	private void skipPayloadMetaData(ByteArrayInputStream is) throws IOException {
-		for(int i = 0; i < PAYLOAD_META_DATA_INIT_BYTES.length; i++) {
-			is.read();
-		}
-	}
+    /***************************************************************************************/
+    //we padding payload with clean bytes which then will be used for metadata writing
+    private static final byte[] PAYLOAD_META_DATA_INIT_BYTES = {0};
 
-	private void initPayloadMetaData(ByteArrayOutputStream baos) throws IOException {
-		baos.write(PAYLOAD_META_DATA_INIT_BYTES);
-	}
+    //for now meta data is only one byte
+    private static final int INFRA_PART_BYTE = 0;
+
+    private static final int IS_SENSITIVE = 1;
+
+    private void setSensitive(Payload payload) {
+        payload.getData()[INFRA_PART_BYTE] = IS_SENSITIVE;
+    }
+
+    private boolean isSensitive(Payload payload) {
+        return payload.getData()[INFRA_PART_BYTE] == IS_SENSITIVE;
+    }
+
+    private void skipPayloadMetaData(ByteArrayInputStream is)  {
+        for (int i = 0; i < PAYLOAD_META_DATA_INIT_BYTES.length; i++) {
+            is.read();
+        }
+    }
+
+    private void skipEncryptionMarker(ByteArrayInputStream is)  {
+        for (int i = 0; i < ENCRYPTION_MARKER.length; i++) {
+            is.read();
+        }
+    }
+
+    private void initPayloadMetaData(ByteArrayOutputStream baos) throws IOException {
+        baos.write(PAYLOAD_META_DATA_INIT_BYTES);
+    }
+
 }
