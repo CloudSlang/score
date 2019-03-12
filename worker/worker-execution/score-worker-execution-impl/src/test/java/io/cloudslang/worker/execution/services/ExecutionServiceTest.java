87f509b9c3d3958b91f0726b028159f6678bee72
Radu Cimpean <radu.cimpean@microfocus.com>
2019-03-12 09:11:41 +0200

    test that reaching sequential operation pauses the execution (#175)
    * test that reaching sequential operation pauses the execution
    
    Signed-off-by: cimpeanr <radu.cimpean@microfocus.com>
    
    * Update ExecutionServiceTest.java
    
    indentation fix
    
    * code review processing
    
    Signed-off-by: cimpeanr <radu.cimpean@microfocus.com>
    
    * proper indentation
    
    Signed-off-by: cimpeanr <radu.cimpean@microfocus.com>
    
    * additional check on pauseResumeService
    
    Signed-off-by: cimpeanr <radu.cimpean@microfocus.com>

--- worker/worker-execution/score-worker-execution-impl/src/test/java/io/cloudslang/worker/execution/services/ExecutionServiceTest.java
+++ worker/worker-execution/score-worker-execution-impl/src/test/java/io/cloudslang/worker/execution/services/ExecutionServiceTest.java
@@ -16,6 +16,8 @@
 
 package io.cloudslang.worker.execution.services;
 
+import io.cloudslang.orchestrator.services.CancelExecutionService;
+import io.cloudslang.orchestrator.services.PauseResumeService;
 import io.cloudslang.score.api.ControlActionMetadata;
 import io.cloudslang.score.api.ExecutionPlan;
 import io.cloudslang.score.api.ExecutionStep;
@@ -27,8 +29,6 @@ import io.cloudslang.score.facade.entities.RunningExecutionPlan;
 import io.cloudslang.score.facade.execution.ExecutionStatus;
 import io.cloudslang.score.facade.execution.ExecutionSummary;
 import io.cloudslang.score.facade.execution.PauseReason;
-import io.cloudslang.orchestrator.services.CancelExecutionService;
-import io.cloudslang.orchestrator.services.PauseResumeService;
 import io.cloudslang.worker.execution.reflection.ReflectionAdapter;
 import io.cloudslang.worker.management.WorkerConfigurationService;
 import io.cloudslang.worker.management.services.WorkerRecoveryManager;
@@ -49,7 +49,13 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import static io.cloudslang.score.api.execution.ExecutionParametersConsts.ACTION_TYPE;
+import static io.cloudslang.score.api.execution.ExecutionParametersConsts.SEQUENTIAL;
+import static io.cloudslang.score.facade.execution.PauseReason.SEQUENTIAL_EXECUTION;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -66,8 +72,9 @@ public class ExecutionServiceTest {
 	private static final Long RUNNING_EXE_PLAN_ID = 333L;
 	private static final Long EXECUTION_STEP_1_ID = 1L;
 	private static final Long EXECUTION_STEP_2_ID = 2L;
-	static final Long EXECUTION_ID_1 = 1111L;
-	static final Long EXECUTION_ID_2 = 2222L;
+	private static final Long EXECUTION_ID_1 = 1111L;
+	private static final Long EXECUTION_ID_2 = 2222L;
+	private static final ControlActionMetadata RUNTIME_EXCEPTION_METADATA = new ControlActionMetadata("classForFailure", "method");
 
 	@Autowired
 	private ExecutionServiceImpl executionService;
@@ -88,7 +95,7 @@ public class ExecutionServiceTest {
 
 	@Test
 	public void handlePausedFlow_NotPausedExecutionTest() throws InterruptedException {
-		Execution exe = new Execution(111L,0L, 0L, new HashMap<String,String>(), null);
+		Execution exe = new Execution(111L, 0L, 0L, new HashMap<String, String>(), null);
 		exe.getSystemContext().setBranchId("branch_id");
 		exe.getSystemContext().put(EventConstants.FLOW_UUID, "flow_uuid");
 
@@ -114,7 +121,7 @@ public class ExecutionServiceTest {
 		boolean result = executionService.handlePausedFlow(exe);
 
 		Mockito.verify(pauseResumeService, VerificationModeFactory.times(1)).writeExecutionObject(executionId, branch_id, exe);
-		Assert.assertTrue(result);
+		assertTrue(result);
 	}
 
 	@Test
@@ -141,11 +148,11 @@ public class ExecutionServiceTest {
 
 		Mockito.verify(pauseResumeService, VerificationModeFactory.times(1)).pauseExecution(executionId, branch_id, PauseReason.USER_PAUSED);
 		Mockito.verify(pauseResumeService, VerificationModeFactory.times(1)).writeExecutionObject(executionId, branch_id, exe);
-		Assert.assertTrue(result);
+		assertTrue(result);
 	}
 
 	private Execution getExecutionObjToPause(Long executionId, String branch_id) {
-		Execution exe = new Execution(executionId,0L, 0L, new HashMap<String,String>(), null);
+		Execution exe = new Execution(executionId, 0L, 0L, new HashMap<String, String>(), null);
 		exe.getSystemContext().setBranchId(branch_id);
 		exe.getSystemContext().put(EventConstants.FLOW_UUID, "flow_uuid");
 		//for events
@@ -155,20 +162,70 @@ public class ExecutionServiceTest {
 
 	@Test
 	public void handleCancelledFlowsTest() {
-		Execution exe = new Execution(EXECUTION_ID_1,0L, 0L, new HashMap<String,String>(), null);
+		Execution exe = new Execution(EXECUTION_ID_1, 0L, 0L, new HashMap<String, String>(), null);
 
 		boolean result = executionService.handleCancelledFlow(exe);
 
-		Assert.assertEquals(exe.getPosition(), null);
-		Assert.assertEquals(result, true);
+		assertNull(exe.getPosition());
+		assertTrue(result);
 
-		exe = new Execution(EXECUTION_ID_2,0L, 0L, new HashMap<String,String>(), null);
+		exe = new Execution(EXECUTION_ID_2, 0L, 0L, new HashMap<String, String>(), null);
 
 		result = executionService.handleCancelledFlow(exe);
 
-		Assert.assertEquals(exe.getPosition(), null);
-		Assert.assertEquals(result, true);
+		assertNull(exe.getPosition());
+		assertTrue(result);
 
+	}
+
+	@Test
+	// branch is running and execution reaches sequential operation -> branch should be paused
+	public void handlePausedFlow_sequentialOperationReached() throws InterruptedException {
+
+		ExecutionStep executionStep = createExecutionStep();
+		Execution execution = createExecution(executionStep);
+		RunningExecutionPlan runningExecutionPlan = createRunningExecutionPlan(executionStep, execution);
+
+		when(workerDbSupportService.readExecutionPlanById(RUNNING_EXE_PLAN_ID)).thenReturn(runningExecutionPlan);
+		when(workerConfigurationService.isExecutionCancelled(EXECUTION_ID_1)).thenReturn(false);
+
+		executionService.execute(execution);
+		//position is still 0
+		Assert.assertEquals(0, execution.getPosition().longValue());
+
+		//running execution plan id has not changed as result of not navigating
+		Assert.assertEquals(RUNNING_EXE_PLAN_ID, execution.getRunningExecutionPlanId());
+		Mockito.verify(pauseResumeService, VerificationModeFactory.times(1)).pauseExecution(any(Long.class), any(String.class), eq(SEQUENTIAL_EXECUTION));
+		Mockito.verify(pauseResumeService, VerificationModeFactory.times(1)).writeExecutionObject(any(Long.class), any(String.class), eq(execution));
+		Mockito.verifyNoMoreInteractions(pauseResumeService);
+	}
+
+	private Execution createExecution(ExecutionStep executionStep) {
+		Execution execution = new Execution(EXECUTION_ID_1, 0L, 0L, new HashMap<String, String>(), null);
+		execution.getSystemContext().put(TempConstants.CONTENT_EXECUTION_STEP, executionStep);
+		Map<String, Serializable> metadata = new HashMap<>();
+		execution.getSystemContext().putMetaData(metadata);
+		return execution;
+	}
+
+	private RunningExecutionPlan createRunningExecutionPlan(ExecutionStep executionStep, Execution execution) {
+		ExecutionPlan executionPlan = new ExecutionPlan();
+		executionPlan.addStep(executionStep);
+		RunningExecutionPlan runningExecutionPlan = new RunningExecutionPlan();
+		runningExecutionPlan.setId(RUNNING_EXE_PLAN_ID);
+		runningExecutionPlan.setExecutionPlan(executionPlan);
+		execution.setRunningExecutionPlanId(runningExecutionPlan.getId());
+		return runningExecutionPlan;
+	}
+
+	private ExecutionStep createExecutionStep() {
+		ExecutionStep executionStep = new ExecutionStep(EXECUTION_STEP_1_ID);
+		HashMap<String, Serializable> actionData = new HashMap<>();
+		actionData.put(ACTION_TYPE, SEQUENTIAL);
+		ControlActionMetadata controlActionMetadata = new ControlActionMetadata("className", "methodName");
+		executionStep.setActionData(actionData);
+		executionStep.setAction(controlActionMetadata);
+		return executionStep;
 	}
 
 	@Test
@@ -176,10 +233,10 @@ public class ExecutionServiceTest {
 		//FromSystemContext
 		ExecutionStep executionStep = new ExecutionStep(EXECUTION_STEP_1_ID);
 
-		Execution exe = new Execution(EXECUTION_ID_1,0L, 0L, new HashMap<String,String>(), null);
+		Execution exe = new Execution(EXECUTION_ID_1, 0L, 0L, new HashMap<String, String>(), null);
 
 		exe.getSystemContext().put(TempConstants.CONTENT_EXECUTION_STEP, executionStep);
-		Map<String,Serializable> metadata = new HashMap<>();
+		Map<String, Serializable> metadata = new HashMap<>();
 		exe.getSystemContext().putMetaData(metadata);
 		ExecutionStep loadedStep = executionService.loadExecutionStep(exe);
 
@@ -196,7 +253,7 @@ public class ExecutionServiceTest {
 
 		executionStep = new ExecutionStep(EXECUTION_STEP_2_ID);
 
-		exe = new Execution(RUNNING_EXE_PLAN_ID, EXECUTION_STEP_2_ID, new HashMap<String,String>());
+		exe = new Execution(RUNNING_EXE_PLAN_ID, EXECUTION_STEP_2_ID, new HashMap<String, String>());
 		exe.getSystemContext().putMetaData(metadata);
 		loadedStep = executionService.loadExecutionStep(exe);
 
@@ -209,32 +266,32 @@ public class ExecutionServiceTest {
 		ExecutionStep executionStep = new ExecutionStep(EXECUTION_STEP_1_ID);
 		executionStep.setActionData(new HashMap<String, Serializable>());
 
-		Execution exe = new Execution(0L, 0L, new HashMap<String,String>());
+		Execution exe = new Execution(0L, 0L, new HashMap<String, String>());
 
 		executionService.executeStep(exe, executionStep);
 
 		Assert.assertEquals(0, exe.getPosition().longValue()); //position is still 0
-		Assert.assertTrue(exe.getSystemContext().hasStepErrorKey()); //there is error in context
+		assertTrue(exe.getSystemContext().hasStepErrorKey()); //there is error in context
 	}
 
 	@Test
 	public void executeNavigationTest() throws InterruptedException {
 		//Test no exception is thrown - all is caught inside
 		ExecutionStep executionStep = new ExecutionStep(EXECUTION_STEP_1_ID);
-		executionStep.setNavigation(new ControlActionMetadata("class", "method"));
+		executionStep.setNavigation(RUNTIME_EXCEPTION_METADATA);
 		executionStep.setNavigationData(new HashMap<String, Serializable>());
 
-		Execution exe = new Execution(0L, 0L, new HashMap<String,String>());
+		Execution exe = new Execution(0L, 0L, new HashMap<String, String>());
 
 		executionService.navigate(exe, executionStep);
 
-		Assert.assertEquals(null, exe.getPosition()); //position was changed to NULL due to exception
-		Assert.assertTrue(exe.getSystemContext().hasStepErrorKey()); //there is error in context
+		assertNull(exe.getPosition()); //position was changed to NULL due to exception
+		assertTrue(exe.getSystemContext().hasStepErrorKey()); //there is error in context
 	}
 
 	@Test
 	public void postExecutionSettingsTest() {
-		Execution exe = new Execution(1111111L,0L, 0L, new HashMap<String,String>(), null);
+		Execution exe = new Execution(1111111L, 0L, 0L, new HashMap<String, String>(), null);
 
 		exe.getSystemContext().put(TempConstants.ACTUALLY_OPERATION_GROUP, "Real_Group");
 		//for events
@@ -271,7 +328,7 @@ public class ExecutionServiceTest {
 			ReflectionAdapter adapter = mock(ReflectionAdapter.class);
 
 			//noinspection unchecked
-			when(adapter.executeControlAction(any(ControlActionMetadata.class), any(Map.class))).thenThrow(RuntimeException.class);
+			when(adapter.executeControlAction(eq(RUNTIME_EXCEPTION_METADATA), any(Map.class))).thenThrow(RuntimeException.class);
 
 			return adapter;
 		}
