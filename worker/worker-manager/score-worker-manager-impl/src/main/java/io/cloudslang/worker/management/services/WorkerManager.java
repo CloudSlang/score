30721726d28f803e54969f06d094af41007943b1
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-03-27 09:53:05 +0200

    Configure future cancellation based on a system property
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/WorkerManager.java
+++ worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/WorkerManager.java
@@ -46,89 +46,91 @@ import java.util.concurrent.TimeUnit;
 
 import static ch.lambdaj.Lambda.max;
 import static ch.lambdaj.Lambda.on;
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.parseBoolean;
+import static java.lang.System.getProperty;
 
-/**
- * Created with IntelliJ IDEA.
- * User: kravtsov
- * Date: 20/11/12
- * Time: 11:02
- */
+
 public class WorkerManager implements ApplicationListener, EndExecutionCallback, WorkerRecoveryListener {
 
-	private static final int KEEP_ALIVE_FAIL_LIMIT = 5;
-	private static final String DOTNET_PATH = System.getenv("WINDIR") + "/Microsoft.NET/Framework";
-	private static final Logger logger = Logger.getLogger(WorkerManager.class);
+    private static final Logger logger = Logger.getLogger(WorkerManager.class);
+    private static final int KEEP_ALIVE_FAIL_LIMIT = 5;
+    private static final String DOTNET_PATH = System.getenv("WINDIR") + "/Microsoft.NET/Framework";
 
-	@Resource
-	private String workerUuid;
+    @Resource
+    private String workerUuid;
 
-	@Autowired
-	protected WorkerNodeService workerNodeService;
+    @Autowired
+    protected WorkerNodeService workerNodeService;
 
-	@Autowired
-	private EngineVersionService engineVersionService;
+    @Autowired
+    private EngineVersionService engineVersionService;
 
-	@Autowired
-	protected WorkerConfigurationService workerConfigurationService;
+    @Autowired
+    protected WorkerConfigurationService workerConfigurationService;
 
-	@Autowired
-	protected WorkerRecoveryManager recoveryManager;
+    @Autowired
+    protected WorkerRecoveryManager recoveryManager;
 
-	@Autowired
-	protected WorkerVersionService workerVersionService;
+    @Autowired
+    protected WorkerVersionService workerVersionService;
 
-	private LinkedBlockingQueue<Runnable> inBuffer;
+    private LinkedBlockingQueue<Runnable> inBuffer;
 
-	@Autowired
-	@Qualifier("numberOfExecutionThreads")
-	private Integer numberOfThreads;
+    @Autowired
+    @Qualifier("numberOfExecutionThreads")
+    private Integer numberOfThreads;
 
-	@Autowired(required = false)
-	@Qualifier("initStartUpSleep")
-	private Long initStartUpSleep = 15*1000L; // by default 15 seconds
+    @Autowired(required = false)
+    @Qualifier("initStartUpSleep")
+    private Long initStartUpSleep = 15 * 1000L; // by default 15 seconds
 
-	@Autowired(required = false)
-	@Qualifier("maxStartUpSleep")
-	private Long maxStartUpSleep = 10*60*1000L; // by default 10 minutes
+    @Autowired(required = false)
+    @Qualifier("maxStartUpSleep")
+    private Long maxStartUpSleep = 10 * 60 * 1000L; // by default 10 minutes
 
     private int keepAliveFailCount = 0;
 
-	private ExecutorService executorService;
+    private ExecutorService executorService;
 
-	private ConcurrentMap<Long, Queue<Future>> mapOfRunningTasks;
+    private ConcurrentMap<Long, Queue<Future>> mapOfRunningTasks;
 
-	private volatile boolean endOfInit = false;
+    private volatile boolean endOfInit = false;
 
     private volatile boolean initStarted = false;
 
-	private boolean up = false;
+    private boolean up = false;
 
     private volatile int threadPoolVersion = 0;
 
-	@PostConstruct
-	private void init() {
-		logger.info("Initialize worker with UUID: " + workerUuid);
-		System.setProperty("worker.uuid", workerUuid); //do not remove!!!
+    private boolean newCancelBehaviour;
+
+    @PostConstruct
+    private void init() {
+        logger.info("Initialize worker with UUID: " + workerUuid);
+        System.setProperty("worker.uuid", workerUuid); //do not remove!!!
         inBuffer = new LinkedBlockingQueue<>();
 
-		executorService = new ThreadPoolExecutor(numberOfThreads,
-				numberOfThreads,
-				Long.MAX_VALUE, TimeUnit.NANOSECONDS,
-				inBuffer,
+        executorService = new ThreadPoolExecutor(numberOfThreads,
+                numberOfThreads,
+                Long.MAX_VALUE, TimeUnit.NANOSECONDS,
+                inBuffer,
                 new WorkerThreadFactory((++threadPoolVersion) + "_WorkerExecutionThread"));
 
-		mapOfRunningTasks = new ConcurrentHashMap<>(numberOfThreads);
-	}
+        mapOfRunningTasks = new ConcurrentHashMap<>(numberOfThreads);
 
-	public void addExecution(long executionId, Runnable runnable) {
-		// Since we can offer to thread pool queue from SimpleExecutionRunnable run method
+        newCancelBehaviour = parseBoolean(getProperty("enable.new.cancel.execution", FALSE.toString()));
+    }
+
+    public void addExecution(long executionId, Runnable runnable) {
+        // Since we can offer to thread pool queue from SimpleExecutionRunnable run method
         // it is possible we will have step x + 1 of an execution plan that is in the map of running tasks,
         // but step x did not yet clean itself from the map
         Future future = executorService.submit(runnable);
         mapOfRunningTasks.merge(executionId, newQueue(future), this::addLists);
-	}
+    }
 
-	private Queue<Future> newQueue(Future future) {
+    private Queue<Future> newQueue(Future future) {
         Queue<Future> queue = new LinkedList<>();
         queue.offer(future);
         return queue;
@@ -139,35 +141,41 @@ public class WorkerManager implements ApplicationListener, EndExecutionCallback,
         return oldValue;
     }
 
-	@Override
-	public void endExecution(Long executionId) {
+    @Override
+    public void endExecution(Long executionId) {
         mapOfRunningTasks.merge(executionId, new LinkedList<>(),
                 (queue, newValue) -> {
                     queue.poll();
                     return !queue.isEmpty() ? queue : null;
                 }
         );
-	}
+    }
 
-	public int getInBufferSize() {
-		return inBuffer.size();
-	}
+    public int getInBufferSize() {
+        return inBuffer.size();
+    }
 
     @SuppressWarnings("unused")
-    //scheduled in scoreWorkerSchedulerContext.xml
-    public void interruptCanceledExecutions(){
-        for(Long executionId  : mapOfRunningTasks.keySet()){
-            if(workerConfigurationService.isExecutionCancelled(executionId)){
-                Collection<Future> futures = mapOfRunningTasks.get(executionId);
-				for(Future future : futures) {
-					future.cancel(true);
-				}
-			}
+    // Scheduled in scoreWorkerSchedulerContext.xml
+    public void interruptCanceledExecutions() {
+        try {
+            if (!newCancelBehaviour) { // For backward compatibility
+                for (Long executionId : mapOfRunningTasks.keySet()) {
+                    if (workerConfigurationService.isExecutionCancelled(executionId)) {
+                        Collection<Future> futures = mapOfRunningTasks.get(executionId);
+                        for (Future future : futures) {
+                            future.cancel(true);
+                        }
+                    }
+                }
+            }
+        } catch (Exception exc) {
+            logger.error("Could not stop cancelled executions: ", exc);
         }
     }
 
-	@SuppressWarnings("unused")
-    //scheduled in xml
+    @SuppressWarnings("unused")
+    // Scheduled in xml
     public void workerKeepAlive() {
         if (!recoveryManager.isInRecovery()) {
             if (endOfInit) {
@@ -175,7 +183,7 @@ public class WorkerManager implements ApplicationListener, EndExecutionCallback,
                     String newWrv = workerNodeService.keepAlive(workerUuid);
                     String currentWrv = recoveryManager.getWRV();
                     //do not update it!!! if it is different than we have - restart worker (clean state)
-                    if(!currentWrv.equals(newWrv)){
+                    if (!currentWrv.equals(newWrv)) {
                         logger.warn("Got new WRV from Orchestrator during keepAlive(). Going to reload...");
                         recoveryManager.doRecovery();
                     }
@@ -183,29 +191,31 @@ public class WorkerManager implements ApplicationListener, EndExecutionCallback,
                 } catch (Exception e) {
                     keepAliveFailCount++;
                     logger.error("Could not send keep alive to Central, keepAliveFailCount = " + keepAliveFailCount, e);
-                    if(keepAliveFailCount >= KEEP_ALIVE_FAIL_LIMIT){
-                        logger.error("Failed sending keepAlive for " + KEEP_ALIVE_FAIL_LIMIT + " times. Invoking worker internal recovery...");
+                    if (keepAliveFailCount >= KEEP_ALIVE_FAIL_LIMIT) {
+                        logger.error("Failed sending keepAlive for " + KEEP_ALIVE_FAIL_LIMIT
+                                + " times. Invoking worker internal recovery...");
                         recoveryManager.doRecovery();
                     }
                 }
             }
+        } else {
+            if (logger.isDebugEnabled()) {
+                logger.debug("worker waits for recovery");
+            }
         }
-        else {
-            if (logger.isDebugEnabled()) logger.debug("worker waits for recovery");
+    }
+
+    @SuppressWarnings("unused") // called by scheduler
+    public void logStatistics() {
+        if (logger.isDebugEnabled()) {
+            logger.debug("InBuffer size: " + getInBufferSize());
+            logger.debug("Running task size: " + mapOfRunningTasks.size());
         }
-	}
+    }
 
-	@SuppressWarnings("unused") // called by scheduler
-	public void logStatistics() {
-		if (logger.isDebugEnabled()) {
-			logger.debug("InBuffer size: " + getInBufferSize());
-			logger.debug("Running task size: " + mapOfRunningTasks.size());
-		}
-	}
-
-	public String getWorkerUuid() {
-		return workerUuid;
-	}
+    public String getWorkerUuid() {
+        return workerUuid;
+    }
 
     public int getRunningTasksCount() {
         return mapOfRunningTasks.size();
@@ -216,94 +226,96 @@ public class WorkerManager implements ApplicationListener, EndExecutionCallback,
     }
 
     @Override
-	public void onApplicationEvent(final ApplicationEvent applicationEvent) {
-		if (applicationEvent instanceof ContextRefreshedEvent && !initStarted) {
-			doStartup();
-		} else if (applicationEvent instanceof ContextClosedEvent) {
-			doShutdown();
-		}
-	}
-
-	private void doStartup() {
-		new Thread(new Runnable() {
-			@Override public void run() {
-				initStarted = true;
-				long sleep = initStartUpSleep;
-				boolean shouldRetry = true;
-				while (shouldRetry) {
-					try {
-						String newWrv = workerNodeService.up(workerUuid, workerVersionService.getWorkerVersion(), workerVersionService.getWorkerVersionId());
-						recoveryManager.setWRV(newWrv); //we do set of WRV here and in doRecovery() only!!! not in keepalive!!!
-						shouldRetry = false;
-						logger.info("Worker is up");
-					} catch (Exception ex) {
-						logger.error("Worker failed on start up, will retry in a " + sleep / 1000 + " seconds", ex);
-						try {
-							Thread.sleep(sleep);
-						} catch (InterruptedException iex) {/*do nothing*/}
-						sleep = Math.min(maxStartUpSleep, sleep * 2); // double the sleep time until max 10 minute
-					}
-				}
-
-				endOfInit = true;
-
-				//Check that this Worker is in the same version as engine - if not - stay idle
-				String engineVersionId = engineVersionService.getEngineVersionId();
-				if(workerVersionService.getWorkerVersionId().equals(engineVersionId)){
-
-					//mark that worker is up and its recovery is ended - only now we can start asking for messages from queue
-					up = true;
-					workerConfigurationService.setEnabled(true);
-					workerNodeService.updateEnvironmentParams(workerUuid,
-							System.getProperty("os.name"),
-							System.getProperty("java.version"),
-							resolveDotNetVersion());
-				}
-				else{
-					logger.warn("Worker's version is not equal to engine version. Won't be able to start processing flows!");
-				}
-			}
-		}).start();
-	}
-
-	private void doShutdown() {
-		endOfInit = false;
-		initStarted = false;
-		workerConfigurationService.setEnabled(false);
-		up = false;
-		logger.info("The worker is down");
-	}
-
-	protected static String resolveDotNetVersion() {
-		File dotNetHome = new File(DOTNET_PATH);
-		if(dotNetHome.isDirectory()) {
-			File[] versionFolders = dotNetHome.listFiles(new FileFilter() {
-
-				@Override
-				public boolean accept(File file) {
-					return file.isDirectory() && file.getName().startsWith("v");
-				}
-			});
-			if(!ArrayUtils.isEmpty(versionFolders)) {
-				String maxVersion = max(versionFolders, on(File.class).getName()).substring(1);
-				return maxVersion.substring(0, 1) + ".x";
-			}
-		}
-		return "N/A";
-	}
-
-	public boolean isUp() {
-		return up;
+    public void onApplicationEvent(final ApplicationEvent applicationEvent) {
+        if (applicationEvent instanceof ContextRefreshedEvent && !initStarted) {
+            doStartup();
+        } else if (applicationEvent instanceof ContextClosedEvent) {
+            doShutdown();
+        }
     }
 
-    public synchronized boolean isFromCurrentThreadPool(String threadName){
-        if(threadName.startsWith(String.valueOf(threadPoolVersion))){
-            if(logger.isDebugEnabled()){
+    private void doStartup() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                initStarted = true;
+                long sleep = initStartUpSleep;
+                boolean shouldRetry = true;
+                while (shouldRetry) {
+                    try {
+                        String newWrv = workerNodeService.up(workerUuid, workerVersionService.getWorkerVersion(),
+                                workerVersionService.getWorkerVersionId());
+                        recoveryManager
+                                .setWRV(newWrv); //we do set of WRV here and in doRecovery() only!!! not in keepalive!!!
+                        shouldRetry = false;
+                        logger.info("Worker is up");
+                    } catch (Exception ex) {
+                        logger.error("Worker failed on start up, will retry in a " + sleep / 1000 + " seconds", ex);
+                        try {
+                            Thread.sleep(sleep);
+                        } catch (InterruptedException iex) {/*do nothing*/}
+                        sleep = Math.min(maxStartUpSleep, sleep * 2); // double the sleep time until max 10 minute
+                    }
+                }
+
+                endOfInit = true;
+
+                //Check that this Worker is in the same version as engine - if not - stay idle
+                String engineVersionId = engineVersionService.getEngineVersionId();
+                if (workerVersionService.getWorkerVersionId().equals(engineVersionId)) {
+
+                    //mark that worker is up and its recovery is ended - only now we can start asking for messages from queue
+                    up = true;
+                    workerConfigurationService.setEnabled(true);
+                    workerNodeService.updateEnvironmentParams(workerUuid,
+                            getProperty("os.name"),
+                            getProperty("java.version"),
+                            resolveDotNetVersion());
+                } else {
+                    logger.warn(
+                            "Worker's version is not equal to engine version. Won't be able to start processing flows!");
+                }
+            }
+        }).start();
+    }
+
+    private void doShutdown() {
+        endOfInit = false;
+        initStarted = false;
+        workerConfigurationService.setEnabled(false);
+        up = false;
+        logger.info("The worker is down");
+    }
+
+    protected static String resolveDotNetVersion() {
+        File dotNetHome = new File(DOTNET_PATH);
+        if (dotNetHome.isDirectory()) {
+            File[] versionFolders = dotNetHome.listFiles(new FileFilter() {
+
+                @Override
+                public boolean accept(File file) {
+                    return file.isDirectory() && file.getName().startsWith("v");
+                }
+            });
+            if (!ArrayUtils.isEmpty(versionFolders)) {
+                String maxVersion = max(versionFolders, on(File.class).getName()).substring(1);
+                return maxVersion.substring(0, 1) + ".x";
+            }
+        }
+        return "N/A";
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public synchronized boolean isFromCurrentThreadPool(String threadName) {
+        if (threadName.startsWith(String.valueOf(threadPoolVersion))) {
+            if (logger.isDebugEnabled()) {
                 logger.debug("Current thread is from current thread pool");
             }
             return true;
-        }
-        else {
+        } else {
             logger.warn("Current thread is NOT from current thread pool!!!");
             return false;
         }
@@ -324,22 +336,21 @@ public class WorkerManager implements ApplicationListener, EndExecutionCallback,
 //        implementations will cancel via {@link Thread#interrupt}, so any
 //        task that fails to respond to interrupts may never terminate.
         try {
-            synchronized (this){
+            synchronized (this) {
                 executorService.shutdownNow(); //shutting down current running threads
                 threadPoolVersion++;           //updating the thread pool version to a new one - so current running threads will exit
-                logger.warn("Worker is in doRecovery(). Cleaning state and cancelling running tasks. It may take up to 30 seconds...");
+                logger.warn(
+                        "Worker is in doRecovery(). Cleaning state and cancelling running tasks. It may take up to 30 seconds...");
             }
 
             boolean finished = executorService.awaitTermination(30, TimeUnit.SECONDS);
 
-            if(finished){
+            if (finished) {
                 logger.warn("Worker succeeded to cancel running tasks during doRecovery().");
-            }
-            else {
+            } else {
                 logger.warn("Not all running tasks responded to cancel.");
             }
-        }
-        catch (InterruptedException ex){/*ignore*/}
+        } catch (InterruptedException ex) {/*ignore*/}
 
         mapOfRunningTasks.clear();
 
