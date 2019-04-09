9207eb3e53e427c7850c330cb0fe7ad0693253c7
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-04-09 13:37:46 +0300

    More fixes
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- engine/queue/score-queue-impl/src/main/java/io/cloudslang/engine/queue/services/assigner/strategies/RoundRobinStrategy.java
+++ engine/queue/score-queue-impl/src/main/java/io/cloudslang/engine/queue/services/assigner/strategies/RoundRobinStrategy.java
@@ -18,7 +18,6 @@ package io.cloudslang.engine.queue.services.assigner.strategies;
 import io.cloudslang.engine.queue.services.assigner.ChooseWorkerStrategy;
 
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.TimeUnit;
 
 import static com.google.common.cache.CacheBuilder.newBuilder;
 import static io.cloudslang.engine.queue.enums.AssignStrategy.RANDOM;
@@ -26,16 +25,19 @@ import static io.cloudslang.engine.queue.enums.AssignStrategy.ROUND_ROBIN;
 import static io.cloudslang.engine.queue.enums.AssignStrategy.getAssignedStrategy;
 import static io.cloudslang.engine.queue.services.assigner.ExecutionAssignerServiceImpl.WORKER_MESSAGE_ASSIGNMENT_POLICY_KEY;
 import static java.lang.Integer.getInteger;
+import static java.util.concurrent.TimeUnit.HOURS;
 
 public class RoundRobinStrategy implements ChooseWorkerStrategy {
 
+    private static final int SEVEN_DAYS_IN_HOURS = 7 * 24;
     private static final int CACHE_MAX_ENTRIES = getInteger("worker.roundRobin.activeGroupAlias.maxSize", 10_000);
     private static final int MAX_CONCURRENCY_LEVEL = getInteger("worker.roundRobin.concurrencyLevel", 16);
+    private static final int EXPIRE_NUMBER_OF_HOURS = getInteger("worker.roundRobin.expireAfterAccess", SEVEN_DAYS_IN_HOURS);
 
     private static final ConcurrentMap<String, Integer> groupAliasLatestWorkerMap = newBuilder()
             .maximumSize(isStrategyInUse() ? CACHE_MAX_ENTRIES : 0)
             .concurrencyLevel(MAX_CONCURRENCY_LEVEL)
-            .expireAfterAccess(7, TimeUnit.DAYS)
+            .expireAfterAccess(EXPIRE_NUMBER_OF_HOURS, HOURS)
             .<String, Integer>build().asMap();
 
     @Override
