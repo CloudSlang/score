08078bbb71445c97a0dfd946e5704331d93b9366
cosmaa <alexandru.cosma@microfocus.com>
2019-04-10 14:03:40 +0300

    default group

--- worker/worker-execution/score-worker-execution-impl/src/main/java/io/cloudslang/worker/execution/services/ExecutionServiceImpl.java
+++ worker/worker-execution/score-worker-execution-impl/src/main/java/io/cloudslang/worker/execution/services/ExecutionServiceImpl.java
@@ -140,7 +140,7 @@ public final class ExecutionServiceImpl implements ExecutionService {
             }
             if ((!execution.getSystemContext().hasStepErrorKey()) && currStep.getActionData().get(ACTION_TYPE) != null &&
                     currStep.getActionData().get(ACTION_TYPE).toString().equalsIgnoreCase(SEQUENTIAL)) {
-                if (!robotConnectionState.isRobotRunning(execution.getGroupName())) {
+                if (!robotConnectionState.isRobotRunning("Default")) {
                     pauseFlow(PauseReason.ROBOT_NOT_AVAILABLE, execution);
                 } else {
                     pauseFlow(PauseReason.SEQUENTIAL_EXECUTION, execution);
@@ -309,7 +309,7 @@ public final class ExecutionServiceImpl implements ExecutionService {
                 // we pause the branch because the Parent was user-paused (see findPauseReason)
                 pauseService.pauseExecution(executionId, branchId, reason); // this creates a DB record for this branch, as Pending-paused
             }
-        } else if (reason.equals(PauseReason.SEQUENTIAL_EXECUTION)) {
+        } else if (reason.equals(PauseReason.SEQUENTIAL_EXECUTION) || reason.equals(PauseReason.ROBOT_NOT_AVAILABLE)) {
             pauseService.pauseExecution(executionId, branchId, reason);
         }
         addPauseEvent(systemContext);
