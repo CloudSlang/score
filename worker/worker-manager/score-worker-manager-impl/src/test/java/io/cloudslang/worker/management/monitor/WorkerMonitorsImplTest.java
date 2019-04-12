e0c0f80cccbbbcd7a8e1c46a7fe59ef91807ee22
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-04-12 10:52:58 +0300

    Fixing more tests
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- worker/worker-manager/score-worker-manager-impl/src/test/java/io/cloudslang/worker/management/monitor/WorkerMonitorsImplTest.java
+++ worker/worker-manager/score-worker-manager-impl/src/test/java/io/cloudslang/worker/management/monitor/WorkerMonitorsImplTest.java
@@ -16,17 +16,26 @@
 
 package io.cloudslang.worker.management.monitor;
 
-import io.cloudslang.orchestrator.services.EngineVersionService;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import io.cloudslang.engine.node.services.WorkerNodeService;
 import io.cloudslang.engine.queue.entities.ExecutionMessageConverter;
 import io.cloudslang.engine.queue.services.QueueDispatcherService;
 import io.cloudslang.engine.queue.services.QueueStateIdGeneratorService;
+import io.cloudslang.orchestrator.services.EngineVersionService;
 import io.cloudslang.worker.execution.services.ExecutionService;
 import io.cloudslang.worker.management.WorkerConfigurationService;
-import io.cloudslang.worker.management.services.*;
+import io.cloudslang.worker.management.services.EndExecutionCallback;
+import io.cloudslang.worker.management.services.InBuffer;
+import io.cloudslang.worker.management.services.OutboundBuffer;
+import io.cloudslang.worker.management.services.SimpleExecutionRunnableFactory;
+import io.cloudslang.worker.management.services.SynchronizationManager;
+import io.cloudslang.worker.management.services.WorkerConfigurationUtils;
+import io.cloudslang.worker.management.services.WorkerManager;
+import io.cloudslang.worker.management.services.WorkerMonitorInfoEnum;
+import io.cloudslang.worker.management.services.WorkerRecoveryManager;
+import io.cloudslang.worker.management.services.WorkerVersionService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,8 +44,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.Serializable;
 import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
@@ -103,32 +117,128 @@ public class WorkerMonitorsImplTest {
 
     @Configuration
     public static class MyTestConfig {
-        @Bean public WorkerMonitors scheduledWorkerLoadMonitor() {return new WorkerMonitorsImpl();}
 
-        @Bean public WorkerManager workerManager() {return mock(WorkerManager.class);}
-        @Bean public OutboundBuffer outboundBuffer() {return mock(OutboundBuffer.class);}
-        @Bean public WorkerNodeService workerNodeService() {return mock(WorkerNodeService.class);}
-        @Bean public WorkerConfigurationService workerConfigurationService() {return mock(WorkerConfigurationService.class);}
-        @Bean public WorkerRecoveryManager workerRecoveryManager() {return mock(WorkerRecoveryManager.class);}
-        @Bean public Integer numberOfExecutionThreads() {return 1;}
-        @Bean public Long initStartUpSleep() {return 1L;}
-        @Bean public Long maxStartUpSleep() {return 2L;}
-        @Bean public String workerUuid() {return "1";}
+        @Bean
+        public WorkerMonitors scheduledWorkerLoadMonitor() {
+            return new WorkerMonitorsImpl();
+        }
 
-        @Bean public WorkerMonitor workerMonitor() {return mock(WorkerMonitor.class);}
-        @Bean public InBuffer inBuffer() {return mock(InBuffer.class);}
-        @Bean public QueueDispatcherService queueDispatcherService() {return mock(QueueDispatcherService.class);}
-        @Bean public Integer inBufferCapacity() {return 1;}
-        @Bean public Integer coolDownPollingMillis() {return 1;}
+        @Bean
+        public WorkerManager workerManager() {
+            return mock(WorkerManager.class);
+        }
 
-        @Bean public SimpleExecutionRunnableFactory simpleExecutionRunnableFactory() {return mock(SimpleExecutionRunnableFactory.class);}
-        @Bean public SynchronizationManager synchronizationManager() {return mock(SynchronizationManager.class);}
+        @Bean
+        public OutboundBuffer outboundBuffer() {
+            return mock(OutboundBuffer.class);
+        }
 
-        @Bean public ExecutionService executionService() {return mock(ExecutionService.class);}
-        @Bean public ExecutionMessageConverter executionMessageConverter() {return mock(ExecutionMessageConverter.class);}
-        @Bean public WorkerVersionService workerVersionService() {return mock(WorkerVersionService.class);}
-        @Bean public EngineVersionService engineVersionService() {return mock(EngineVersionService.class);}
-        @Bean public EndExecutionCallback endExecutionCallback() {return mock(EndExecutionCallback.class);}
-        @Bean public QueueStateIdGeneratorService queueStateIdGeneratorService() {return mock(QueueStateIdGeneratorService.class);}
+        @Bean
+        public WorkerNodeService workerNodeService() {
+            return mock(WorkerNodeService.class);
+        }
+
+        @Bean
+        public WorkerConfigurationService workerConfigurationService() {
+            return mock(WorkerConfigurationService.class);
+        }
+
+        @Bean
+        public WorkerRecoveryManager workerRecoveryManager() {
+            return mock(WorkerRecoveryManager.class);
+        }
+
+        @Bean
+        public Integer numberOfExecutionThreads() {
+            return 1;
+        }
+
+        @Bean
+        public Long initStartUpSleep() {
+            return 1L;
+        }
+
+        @Bean
+        public Long maxStartUpSleep() {
+            return 2L;
+        }
+
+        @Bean
+        public String workerUuid() {
+            return "1";
+        }
+
+        @Bean
+        public WorkerMonitor workerMonitor() {
+            return mock(WorkerMonitor.class);
+        }
+
+        @Bean
+        public InBuffer inBuffer() {
+            return mock(InBuffer.class);
+        }
+
+        @Bean
+        public QueueDispatcherService queueDispatcherService() {
+            return mock(QueueDispatcherService.class);
+        }
+
+        @Bean
+        public Integer inBufferCapacity() {
+            return 1;
+        }
+
+        @Bean
+        public Integer coolDownPollingMillis() {
+            return 1;
+        }
+
+        @Bean
+        public SimpleExecutionRunnableFactory simpleExecutionRunnableFactory() {
+            return mock(SimpleExecutionRunnableFactory.class);
+        }
+
+        @Bean
+        public SynchronizationManager synchronizationManager() {
+            return mock(SynchronizationManager.class);
+        }
+
+        @Bean
+        public ExecutionService executionService() {
+            return mock(ExecutionService.class);
+        }
+
+        @Bean
+        public ExecutionMessageConverter executionMessageConverter() {
+            return mock(ExecutionMessageConverter.class);
+        }
+
+        @Bean
+        public WorkerVersionService workerVersionService() {
+            return mock(WorkerVersionService.class);
+        }
+
+        @Bean
+        public EngineVersionService engineVersionService() {
+            return mock(EngineVersionService.class);
+        }
+
+        @Bean
+        public EndExecutionCallback endExecutionCallback() {
+            return mock(EndExecutionCallback.class);
+        }
+
+        @Bean
+        public QueueStateIdGeneratorService queueStateIdGeneratorService() {
+            return mock(QueueStateIdGeneratorService.class);
+        }
+
+        @Bean
+        public WorkerConfigurationUtils workerConfigurationUtils() {
+            WorkerConfigurationUtils workerConfigurationUtils = mock(WorkerConfigurationUtils.class);
+            doReturn(mock(LinkedBlockingQueue.class)).when(workerConfigurationUtils)
+                    .getBlockingQueue(anyInt(), anyInt());
+            return workerConfigurationUtils;
+        }
     }
 }
