b45b0c9896928af7d0728d384e88dac678b14787
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-05-10 14:57:27 +0300

    Undo gzip
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- engine/queue/score-queue-api/src/main/java/io/cloudslang/engine/queue/entities/ExecutionMessageConverter.java
+++ engine/queue/score-queue-api/src/main/java/io/cloudslang/engine/queue/entities/ExecutionMessageConverter.java
@@ -16,148 +16,124 @@
 
 package io.cloudslang.engine.queue.entities;
 
-import io.cloudslang.score.facade.entities.Execution;
 import org.apache.commons.io.IOUtils;
+import io.cloudslang.score.facade.entities.Execution;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
 
-
+/**
+ * Created with IntelliJ IDEA.
+ * User: kravtsov
+ * Date: 20/11/12
+ * Time: 14:35
+ */
 public class ExecutionMessageConverter {
 
-    private static final byte[] ENCRYPTION_MARKER = new byte[]{73, 28, 45, 109};
-    private static final int SIZE = 1024;
+	public static final int SIZE = 1024;
+	@Autowired(required = false)
+	private SensitiveDataHandler sensitiveDataHandler;
 
-    @Autowired(required = false)
-    private SensitiveDataHandler sensitiveDataHandler;
+	public <T> T extractExecution(Payload payload) {
+		return objFromBytes(payload.getData());
+	}
 
-    public <T> T extractExecution(Payload payload) {
-        return objFromBytes(payload.getData());
-    }
+	public Payload createPayload(Execution execution) {
+		return createPayload(execution, false);
+	}
 
-    public Payload createPayload(Execution execution) {
-        return createPayload(execution, false);
-    }
+	public Payload createPayload(Execution execution, boolean setContainsSensitiveData) {
+		Payload payload = new Payload(objToBytes(execution));
+		if(setContainsSensitiveData || checkContainsSensitiveData(execution)) {
+			setSensitive(payload);
+		}
+		return payload;
+	}
 
-    public Payload createPayload(Execution execution, boolean setContainsSensitiveData) {
-        Payload payload = new Payload(objToBytes(execution));
-        if (setContainsSensitiveData || checkContainsSensitiveData(execution)) {
-            setSensitive(payload);
-        }
-        return payload;
-    }
+	private boolean checkContainsSensitiveData(Execution execution) {
+		return sensitiveDataHandler != null &&
+				sensitiveDataHandler.containsSensitiveData(execution.getSystemContext(), execution.getContexts());
+	}
 
-    private boolean checkContainsSensitiveData(Execution execution) {
-        return sensitiveDataHandler != null &&
-                sensitiveDataHandler.containsSensitiveData(execution.getSystemContext(), execution.getContexts());
-    }
+	public boolean containsSensitiveData(Payload payload) {
+		return isSensitive(payload);
+	}
 
-    public boolean containsSensitiveData(Payload payload) {
-        return isSensitive(payload);
-    }
+	private <T> T objFromBytes(byte[] bytes) {
+		ObjectInputStream ois = null;
+		try {
+			//2 Buffers are added to increase performance
+			ByteArrayInputStream is = new ByteArrayInputStream(bytes);
 
-    private boolean areBytesCompressed(byte[] bytes) {
-        // sensitive is 1 byte, Encryption marker is 4 bytes so compressed is at least 5 in length
-        if (bytes.length < 5) {
-            return false;
-        }
+			skipPayloadMetaData(is);
 
-        // We considered encrypted if ENCRYPTION_MARKER starts in bytes from index 1.
-        for (int i = 0; i < ENCRYPTION_MARKER.length; i++) {
-            if (ENCRYPTION_MARKER[i] != bytes[i + 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
+			BufferedInputStream bis = new BufferedInputStream(is);
+			ois = new ObjectInputStream(bis);
 
-    private <T> T objFromBytes(byte[] bytes) {
-        ObjectInputStream ois = null;
-        try {
-            if (areBytesCompressed(bytes)) { // Compressed object
-                ByteArrayInputStream is = new ByteArrayInputStream(bytes);
-                skipPayloadMetaData(is);
-                skipEncryptionMarker(is);
+			//noinspection unchecked
+			return (T)ois.readObject();
+		}
+		catch(IOException | ClassNotFoundException ex) {
+			throw new RuntimeException("Failed to read execution plan from byte[]. Error: ", ex);
+		}
+		finally {
+			IOUtils.closeQuietly(ois);
+		}
 
-                ois = new ObjectInputStream(new GZIPInputStream(is));
+	}
 
-                // noinspection unchecked
-                return (T) ois.readObject();
-            } else { // Uncompressed object, use legacy implementation
-                // 2 buffers are added to increase performance
-                ByteArrayInputStream is = new ByteArrayInputStream(bytes);
-                skipPayloadMetaData(is);
+	private byte[] objToBytes(Object obj){
+		ObjectOutputStream oos = null;
+		try {
+			ByteArrayOutputStream bout = new ByteArrayOutputStream(SIZE);
 
-                ois = new ObjectInputStream(new BufferedInputStream(is));
+			initPayloadMetaData(bout);
 
-                // noinspection unchecked
-                return (T) ois.readObject();
-            }
-        } catch (IOException | ClassNotFoundException ex) {
-            throw new RuntimeException("Failed to read execution plan from byte[]. Error: ", ex);
-        } finally {
-            IOUtils.closeQuietly(ois);
-        }
-    }
+			BufferedOutputStream bos = new BufferedOutputStream(bout);
+			oos = new ObjectOutputStream(bos);
 
-    private byte[] objToBytes(Object obj) {
-        try (ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(SIZE)) {
-            addPayloadMetadataBytes(byteArrayOutputStream);
-            addEncryptionMarker(byteArrayOutputStream);
+			oos.writeObject(obj);
+			oos.flush();
 
-            try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new GZIPOutputStream(byteArrayOutputStream))) {
-                objectOutputStream.writeObject(obj);
-                objectOutputStream.flush();
-            }
-            // After close of gzip stream to make sure all bytes are flushed
-            return byteArrayOutputStream.toByteArray();
-        } catch (IOException ex) {
-            throw new RuntimeException("Failed to serialize execution plan. Error: ", ex);
-        }
-    }
+			return bout.toByteArray();
+		}
+		catch(IOException ex) {
+			throw new RuntimeException("Failed to serialize execution plan. Error: ", ex);
+		} finally {
+			IOUtils.closeQuietly(oos);
+		}
+	}
 
-    private void addEncryptionMarker(ByteArrayOutputStream bout) {
-        bout.write(ENCRYPTION_MARKER, 0, ENCRYPTION_MARKER.length);
-    }
+	/***************************************************************************************/
+	//we padding payload with clean bytes which then will be used for metadata writing
+	private static final byte[] PAYLOAD_META_DATA_INIT_BYTES = {0};
 
-    /***************************************************************************************/
-    //we padding payload with clean bytes which then will be used for metadata writing
-    private static final byte[] PAYLOAD_META_DATA_INIT_BYTES = {0};
+	//for now meta data is only one byte
+	private static final int INFRA_PART_BYTE = 0;
 
-    //for now meta data is only one byte
-    private static final int INFRA_PART_BYTE = 0;
+	private static final int IS_SENSITIVE = 1;
 
-    private static final int IS_SENSITIVE = 1;
+	private void setSensitive(Payload payload) {
+		payload.getData()[INFRA_PART_BYTE] = IS_SENSITIVE;
+	}
 
-    private void setSensitive(Payload payload) {
-        payload.getData()[INFRA_PART_BYTE] = IS_SENSITIVE;
-    }
+	private boolean isSensitive(Payload payload) {
+		return payload.getData()[INFRA_PART_BYTE] == IS_SENSITIVE;
+	}
 
-    private boolean isSensitive(Payload payload) {
-        return payload.getData()[INFRA_PART_BYTE] == IS_SENSITIVE;
-    }
+	private void skipPayloadMetaData(ByteArrayInputStream is) throws IOException {
+		for(int i = 0; i < PAYLOAD_META_DATA_INIT_BYTES.length; i++) {
+			is.read();
+		}
+	}
 
-    private void skipPayloadMetaData(ByteArrayInputStream is)  {
-        for (int i = 0; i < PAYLOAD_META_DATA_INIT_BYTES.length; i++) {
-            is.read();
-        }
-    }
-
-    private void skipEncryptionMarker(ByteArrayInputStream is)  {
-        for (int i = 0; i < ENCRYPTION_MARKER.length; i++) {
-            is.read();
-        }
-    }
-
-    private void addPayloadMetadataBytes(ByteArrayOutputStream baos) throws IOException {
-        baos.write(PAYLOAD_META_DATA_INIT_BYTES);
-    }
-
+	private void initPayloadMetaData(ByteArrayOutputStream baos) throws IOException {
+		baos.write(PAYLOAD_META_DATA_INIT_BYTES);
+	}
 }
--- engine/queue/score-queue-api/src/test/java/io/cloudslang/engine/queue/entities/ExecutionMessageConverterTest.java
+++ engine/queue/score-queue-api/src/test/java/io/cloudslang/engine/queue/entities/ExecutionMessageConverterTest.java
@@ -16,32 +16,28 @@
 
 package io.cloudslang.engine.queue.entities;
 
-import io.cloudslang.score.facade.entities.Execution;
-import io.cloudslang.score.lang.SystemContext;
+import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import io.cloudslang.score.lang.SystemContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.anyMapOf;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
+/**
+ * Created with IntelliJ IDEA.
+ * User: kravtsov
+ * Date: 20/11/12
+ * Time: 14:35
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = ExecutionMessageConverterTest.ConfigurationForTest.class)
 public class ExecutionMessageConverterTest {
@@ -51,7 +47,7 @@ public class ExecutionMessageConverterTest {
     private SensitiveDataHandler sensitiveDataHandler;
 
     @Test
-    public void testConverterCompressed() {
+    public void testConverter() throws IOException {
         List<String> names = new ArrayList<>();
         names.add("lala");
         MyExecutionForTest execution = new MyExecutionForTest(111L, 999L, 0L, names);
@@ -59,53 +55,6 @@ public class ExecutionMessageConverterTest {
         Payload payload = executionMessageConverter.createPayload(execution);
 
         MyExecutionForTest afterConvert = executionMessageConverter.extractExecution(payload);
-
-        assertEquals(execution.getPosition(), afterConvert.getPosition());
-        assertEquals(execution.getExecutionId(), afterConvert.getExecutionId());
-        assertEquals(execution.getRunningExecutionPlanId(), afterConvert.getRunningExecutionPlanId());
-    }
-
-    @Test
-    public void testConverterCompressedWithExecution() {
-
-        Execution execution = new Execution(123L, 1L, new HashMap<>());
-        execution.setExecutionId(1L);
-        Payload payload = executionMessageConverter.createPayload(execution);
-
-        Execution afterConvert = executionMessageConverter.extractExecution(payload);
-
-        assertEquals(execution.getPosition(), afterConvert.getPosition());
-        assertEquals(execution.getExecutionId(), afterConvert.getExecutionId());
-        assertEquals(execution.getRunningExecutionPlanId(), afterConvert.getRunningExecutionPlanId());
-    }
-
-    @Test
-    public void testConverterLegacyCaseUncompressed() throws IOException {
-        List<String> names = new ArrayList<>();
-        names.add("1");
-        names.add("2");
-        names.add("3");
-
-        MyExecutionForTest execution = new MyExecutionForTest(222L, 9L, 500L, names);
-
-        Payload payload = createLegacyPayload(execution);
-
-        MyExecutionForTest afterConvert = executionMessageConverter.extractExecution(payload);
-
-        assertEquals(execution.getPosition(), afterConvert.getPosition());
-        assertEquals(execution.getExecutionId(), afterConvert.getExecutionId());
-        assertEquals(execution.getRunningExecutionPlanId(), afterConvert.getRunningExecutionPlanId());
-    }
-
-    @Test
-    public void testConverterLegacyCaseWithExecution() throws IOException {
-
-        Execution execution = new Execution(576767600L, 1L, new HashMap<>());
-        execution.setExecutionId(2L);
-
-        Payload payload = createLegacyPayload(execution);
-
-        Execution afterConvert = executionMessageConverter.extractExecution(payload);
 
         assertEquals(execution.getPosition(), afterConvert.getPosition());
         assertEquals(execution.getExecutionId(), afterConvert.getExecutionId());
@@ -114,50 +63,50 @@ public class ExecutionMessageConverterTest {
 
     @Test
     public void testCreatePayloadAndSensitiveDataHandlerReturnsFalse() {
-        when(sensitiveDataHandler.containsSensitiveData(any(SystemContext.class), anyMapOf(String.class, Serializable.class))).thenReturn(false);
+        when(sensitiveDataHandler.containsSensitiveData(any(SystemContext.class), anyMap())).thenReturn(false);
         List<String> names = new ArrayList<>();
         names.add("lala");
         MyExecutionForTest execution = new MyExecutionForTest(111L, 999L, 0L, names);
 
         Payload payload = executionMessageConverter.createPayload(execution);
         assertFalse(executionMessageConverter.containsSensitiveData(payload));
-        assertEquals(0, payload.getData()[0]);
+        assertTrue(payload.getData()[0] == 0);
 
         payload = executionMessageConverter.createPayload(execution);
         assertFalse(executionMessageConverter.containsSensitiveData(payload));
-        assertEquals(0, payload.getData()[0]);
+        assertTrue(payload.getData()[0] == 0);
 
         payload = executionMessageConverter.createPayload(execution, false);
         assertFalse(executionMessageConverter.containsSensitiveData(payload));
-        assertEquals(0, payload.getData()[0]);
+        assertTrue(payload.getData()[0] == 0);
 
         payload = executionMessageConverter.createPayload(execution, true);
         assertTrue(executionMessageConverter.containsSensitiveData(payload));
-        assertEquals(1, payload.getData()[0]);
+        assertTrue(payload.getData()[0] == 1);
     }
 
     @Test
     public void testCreatePayloadAndSensitiveDataHandlerReturnsTrue() {
-        when(sensitiveDataHandler.containsSensitiveData(any(SystemContext.class), anyMapOf(String.class, Serializable.class))).thenReturn(true);
+        when(sensitiveDataHandler.containsSensitiveData(any(SystemContext.class), anyMap())).thenReturn(true);
         List<String> names = new ArrayList<>();
         names.add("lala");
         MyExecutionForTest execution = new MyExecutionForTest(111L, 999L, 0L, names);
 
         Payload payload = executionMessageConverter.createPayload(execution);
         assertTrue(executionMessageConverter.containsSensitiveData(payload));
-        assertEquals(1, payload.getData()[0]);
+        assertTrue(payload.getData()[0] == 1);
 
         payload = executionMessageConverter.createPayload(execution);
         assertTrue(executionMessageConverter.containsSensitiveData(payload));
-        assertEquals(1, payload.getData()[0]);
+        assertTrue(payload.getData()[0] == 1);
 
         payload = executionMessageConverter.createPayload(execution, false);
         assertTrue(executionMessageConverter.containsSensitiveData(payload));
-        assertEquals(1, payload.getData()[0]);
+        assertTrue(payload.getData()[0] == 1);
 
         payload = executionMessageConverter.createPayload(execution, true);
         assertTrue(executionMessageConverter.containsSensitiveData(payload));
-        assertEquals(1, payload.getData()[0]);
+        assertTrue(payload.getData()[0] == 1);
     }
 
     @Test
@@ -171,16 +120,26 @@ public class ExecutionMessageConverterTest {
         assertTrue(executionMessageConverter.containsSensitiveData(p));
     }
 
-    private Payload createLegacyPayload(Serializable serializable) throws IOException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
-        byteArrayOutputStream.write(0);
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
-            objectOutputStream.writeObject(serializable);
-            objectOutputStream.flush();
-        }
+//    @Test
+//    public void testConverterWithSession() throws IOException {
+//        List<String> names = new ArrayList<>();
+//        names.add("serializableSessionContext");
+//        Execution execution = new Execution(999L, 0L, names);
+//
+//        StepSerializableSessionObject stepSerializableObject = new StepSerializableSessionObject("sessionCounter_1bbd31ec-0531-4180-8b70-a592355ea043");
+//        stepSerializableObject.setValue(1);
+//
+//        execution.getSerializableSessionContext().put("sessionCounter_1bbd31ec-0531-4180-8b70-a592355ea043", stepSerializableObject);
+//
+//        Payload payload = converter.createPayload(execution);
+//
+//        Execution afterConvert = converter.extractExecution(payload);
+//
+//        Assert.assertEquals(execution.getPosition(), afterConvert.getPosition());
+//        Assert.assertEquals(execution.getExecutionId(), afterConvert.getExecutionId());
+//        Assert.assertEquals(execution.getSerializableSessionContext().get("sessionCounter_1bbd31ec-0531-4180-8b70-a592355ea043").getName(), afterConvert.getSerializableSessionContext().get("sessionCounter_1bbd31ec-0531-4180-8b70-a592355ea043").getName());
+//    }
 
-        return new Payload(byteArrayOutputStream.toByteArray());
-    }
 
     @Configuration
     static class ConfigurationForTest {
