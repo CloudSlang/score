a341059a668335a1219fbad86b7fbabea30ec055
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-05-17 10:55:50 +0300

    Fix potential long wait of execution threads because of outbound buffer draining waiting for thread from shared scoreWorkerScheduler
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/OutboundBufferImpl.java
+++ worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/OutboundBufferImpl.java
@@ -21,12 +21,12 @@ import io.cloudslang.engine.queue.entities.ExecutionMessage;
 import io.cloudslang.orchestrator.entities.Message;
 import io.cloudslang.orchestrator.services.OrchestratorDispatcherService;
 import io.cloudslang.worker.management.ExecutionsActivityListener;
-import org.apache.commons.lang.Validate;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -39,188 +39,211 @@ import static ch.lambdaj.Lambda.by;
 import static ch.lambdaj.Lambda.extract;
 import static ch.lambdaj.Lambda.group;
 import static ch.lambdaj.Lambda.on;
+import static org.apache.commons.lang.Validate.notEmpty;
 
 public class OutboundBufferImpl implements OutboundBuffer, WorkerRecoveryListener {
-	private final Logger logger = Logger.getLogger(this.getClass());
 
-    private static long GB = 900000000;//there is JVM overhead, so i will take 10% buffer...
+    private static final Logger logger = Logger.getLogger(OutboundBufferImpl.class);
+    private static long GB = 900000000; //there is JVM overhead, so i will take 10% buffer...
 
-	@Autowired
-	private RetryTemplate retryTemplate;
+    @Autowired
+    private RetryTemplate retryTemplate;
 
-	@Autowired
-	private WorkerRecoveryManager recoveryManager;
+    @Autowired
+    private WorkerRecoveryManager recoveryManager;
 
-	@Autowired
-	private OrchestratorDispatcherService dispatcherService;
+    @Autowired
+    private OrchestratorDispatcherService dispatcherService;
 
     @Resource
     private String workerUuid;
 
     @Autowired
-   	private SynchronizationManager syncManager;
+    private SynchronizationManager syncManager;
 
     @Autowired(required = false)
     private ExecutionsActivityListener executionsActivityListener;
 
-	private List<Message> buffer = new ArrayList<>();
+    private List<Message> buffer = new ArrayList<>();
 
-	private int currentWeight;
+    private int currentWeight;
 
-	private int maxBufferWeight = Integer.getInteger("out.buffer.max.buffer.weight", 30000);
-	private int maxBulkWeight = Integer.getInteger("out.buffer.max.bulk.weight", 1500);
-	private int retryAmount = Integer.getInteger("out.buffer.retry.number", 5);
-	private long retryDelay = Long.getLong("out.buffer.retry.delay", 5000);
+    private int maxBufferWeight = Integer.getInteger("out.buffer.max.buffer.weight", 30000);
+    private int maxBulkWeight = Integer.getInteger("out.buffer.max.bulk.weight", 1500);
+    private int retryAmount = Integer.getInteger("out.buffer.retry.number", 5);
+    private long retryDelay = Long.getLong("out.buffer.retry.delay", 5000);
 
     @PostConstruct
-    public void init(){
+    public void init() {
         maxBufferWeight = Integer.getInteger("out.buffer.max.buffer.weight", defaultBufferCapacity());
         logger.info("maxBufferWeight = " + maxBufferWeight);
     }
 
-	@Override
-	public void put(final Message... messages) throws InterruptedException {
-		Validate.notEmpty(messages, "The array of messages is null or empty");
-        try{
+    @Override
+    public void put(final Message... messages) throws InterruptedException {
+        notEmpty(messages, "The array of messages is null or empty");
+        try {
             syncManager.startPutMessages();
 
-            //We need to check if the current thread was interrupted while waiting for the lock (ExecutionThread or InBufferThread in ackMessages)
-            if(Thread.currentThread().isInterrupted()){
+            // We need to check if the current thread was interrupted while waiting for the lock (ExecutionThread or InBufferThread in ackMessages)
+            if (Thread.currentThread().isInterrupted()) {
                 throw new InterruptedException("Thread was interrupted while waiting on the lock! Exiting...");
-            }
-            else {
-                if(logger.isDebugEnabled())
+            } else {
+                if (logger.isDebugEnabled()) {
                     logger.debug("Current thread was not interrupted! Proceeding to put messages to OutBuffer...");
+                }
             }
 
-            while (currentWeight >= maxBufferWeight){
-                logger.warn("Outbound buffer is full. Waiting...");
+            while (currentWeight >= maxBufferWeight) {
+                logger.info("Outbound buffer is full. Waiting...");
                 syncManager.waitForDrain();
+                logger.info("Outbound buffer was drained. Finished waiting.");
             }
 
-            // in case of multiple messages create a single compound message
+            // In case of multiple messages create a single compound message
             // to make sure that it will be processed in a single transaction
-            Message message = messages.length==1? messages[0]: new CompoundMessage(messages);
+            Message message = messages.length == 1 ? messages[0] : new CompoundMessage(messages);
 
-            //put message into the buffer
+            // Put message into the buffer
             buffer.add(message);
 
             currentWeight += message.getWeight();
-			if (logger.isTraceEnabled()) logger.trace(message.getClass().getSimpleName() + " added to the buffer. " + getStatus());
-		} catch (InterruptedException ex) {
-			logger.warn("Buffer put action was interrupted", ex);
+            if (logger.isTraceEnabled()) {
+                logger.trace(message.getClass().getSimpleName() + " added to the buffer. " + getStatus());
+            }
+        } catch (InterruptedException ex) {
+            logger.warn("Buffer put action was interrupted", ex);
             throw ex;
-		} finally {
-			syncManager.finishPutMessages();
-		}
-	}
+        } finally {
+            syncManager.finishPutMessages();
+        }
+    }
 
-	@Override
-	public void drain() {
-		List<Message> bufferToDrain;
-		try{
+    @Override
+    public void drain() {
+        List<Message> bufferToDrain;
+        try {
             syncManager.startDrain();
-			while (buffer.isEmpty()){
-				if (logger.isDebugEnabled()){
+            while (buffer.isEmpty()) {
+                if (logger.isDebugEnabled()) {
                     logger.debug("buffer is empty. Waiting to drain...");
                 }
-				syncManager.waitForMessages();
-			}
+                syncManager.waitForMessages();
+            }
 
-			if (logger.isDebugEnabled()) logger.debug("buffer is going to be drained. " + getStatus());
+            if (logger.isDebugEnabled()) {
+                logger.debug("buffer is going to be drained. " + getStatus());
+            }
 
-			bufferToDrain = buffer;
-			buffer = new ArrayList<>();
-			currentWeight = 0;
-		} catch (InterruptedException e) {
-			logger.warn("Drain outgoing buffer was interrupted while waiting for messages on the buffer");
-			return;
-		} finally{
-			syncManager.finishDrain();
-		}
+            bufferToDrain = buffer;
+            buffer = new ArrayList<>();
+            currentWeight = 0;
+        } catch (InterruptedException e) {
+            logger.warn("Drain outgoing buffer was interrupted while waiting for messages on the buffer");
+            return;
+        } finally {
+            syncManager.finishDrain();
+        }
 
-		drainInternal(bufferToDrain);
-	}
+        drainInternal(bufferToDrain);
+    }
 
-	private void drainInternal(List<Message> bufferToDrain){
-		List<Message> bulk = new ArrayList<>();
-		int bulkWeight = 0;
-		Map<String,AtomicInteger> logMap = new HashMap<>();
-		try {
-			for (Message message : bufferToDrain) {
-				if (message.getClass().equals(CompoundMessage.class)){
-					bulk.addAll(((CompoundMessage)message).asList());
-				} else {
-					bulk.add(message);
-				}
-				bulkWeight += message.getWeight();
+    private void drainInternal(List<Message> bufferToDrain) {
+        List<Message> bulk = new ArrayList<>();
+        int bulkWeight = 0;
+        Map<String, AtomicInteger> logMap = new HashMap<>();
+        try {
+            for (Message message : bufferToDrain) {
+                if (message.getClass().equals(CompoundMessage.class)) {
+                    bulk.addAll(((CompoundMessage) message).asList());
+                } else {
+                    bulk.add(message);
+                }
+                bulkWeight += message.getWeight();
 
-				if (logger.isDebugEnabled()){
-					if (logMap.get(message.getClass().getSimpleName()) == null) logMap.put(message.getClass().getSimpleName(), new AtomicInteger(1));
-					else logMap.get(message.getClass().getSimpleName()).incrementAndGet();
-				}
+                if (logger.isDebugEnabled()) {
+                    if (logMap.get(message.getClass().getSimpleName()) == null) {
+                        logMap.put(message.getClass().getSimpleName(), new AtomicInteger(1));
+                    } else {
+                        logMap.get(message.getClass().getSimpleName()).incrementAndGet();
+                    }
+                }
 
-				if (bulkWeight > maxBulkWeight){
-					if (logger.isDebugEnabled()) logger.debug("trying to drain bulk: " + logMap.toString() + ", W:" + bulkWeight);
-					drainBulk(bulk);
-					bulk.clear();
-					bulkWeight = 0;
-					logMap.clear();
-				}
-			}
-			// drain the last bulk
-			if (logger.isDebugEnabled()) logger.debug("trying to drain bulk: " + logMap.toString() + ", " + getStatus());
-			drainBulk(bulk);
-		} catch (Exception ex) {
-			logger.error("Failed to drain buffer, invoking worker internal recovery... ", ex);
-			recoveryManager.doRecovery();
-		}
-	}
+                if (bulkWeight > maxBulkWeight) {
+                    if (logger.isDebugEnabled()) {
+                        logger.debug("trying to drain bulk: " + logMap.toString() + ", W:" + bulkWeight);
+                    }
+                    drainBulk(bulk);
+                    bulk.clear();
+                    bulkWeight = 0;
+                    logMap.clear();
+                }
+            }
+            // drain the last bulk
+            if (logger.isDebugEnabled()) {
+                logger.debug("trying to drain bulk: " + logMap.toString() + ", " + getStatus());
+            }
+            drainBulk(bulk);
+        } catch (Exception ex) {
+            logger.error("Failed to drain buffer, invoking worker internal recovery... ", ex);
+            recoveryManager.doRecovery();
+        }
+    }
 
-	private List<Message> optimize(List<Message> messages){
-		long t = System.currentTimeMillis();
-		List<Message> result = new ArrayList<>();
+    private List<Message> optimize(List<Message> messages) {
+        long t = System.currentTimeMillis();
+        List<Message> result = new ArrayList<>();
 
-		Group<Message> groups = group(messages, by(on(Message.class).getId()));
-		for (Group<Message> group :groups.subgroups()){
-			result.addAll(group.first().shrink(group.findAll()));
-		}
+        Group<Message> groups = group(messages, by(on(Message.class).getId()));
+        for (Group<Message> group : groups.subgroups()) {
+            result.addAll(group.first().shrink(group.findAll()));
+        }
 
-		if (logger.isDebugEnabled()) logger.debug("bulk optimization result: " + messages.size() + " -> " + result.size() + " in " + (System.currentTimeMillis()-t) + " ms");
+        if (logger.isDebugEnabled()) {
+            logger.debug("bulk optimization result: " + messages.size() + " -> " + result.size() + " in " + (
+                    System.currentTimeMillis() - t) + " ms");
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private void drainBulk(List<Message> bulkToDrain){
-		long t = System.currentTimeMillis();
-		final List<Message> optimizedBulk = optimize(bulkToDrain);
+    private void drainBulk(List<Message> bulkToDrain) {
+        long t = System.currentTimeMillis();
+        final List<Message> optimizedBulk = optimize(bulkToDrain);
         //Bulk number is the same for all retries! This is done to prevent duplications when we insert with retries
         final String bulkNumber = UUID.randomUUID().toString();
 
-		retryTemplate.retry(retryAmount, retryDelay, new RetryTemplate.RetryCallback() {
-			@Override
-			public void tryOnce() {
+        retryTemplate.retry(retryAmount, retryDelay, new RetryTemplate.RetryCallback() {
+            @Override
+            public void tryOnce() {
                 String wrv = recoveryManager.getWRV();
-                if (logger.isDebugEnabled()) logger.debug("Dispatch start with bulk number: " + bulkNumber);
-				dispatcherService.dispatch(optimizedBulk, bulkNumber, wrv, workerUuid);
-                if (executionsActivityListener != null) {
-                    executionsActivityListener.onHalt(extract(optimizedBulk, on(ExecutionMessage.class).getExecStateId()));
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Dispatch start with bulk number: " + bulkNumber);
                 }
-                if (logger.isDebugEnabled()) logger.debug("Dispatch end with bulk number: " + bulkNumber);
-			}
-		});
-		if (logger.isDebugEnabled()) logger.debug("bulk was drained in " + (System.currentTimeMillis()-t) + " ms");
-	}
+                dispatcherService.dispatch(optimizedBulk, bulkNumber, wrv, workerUuid);
+                if (executionsActivityListener != null) {
+                    executionsActivityListener
+                            .onHalt(extract(optimizedBulk, on(ExecutionMessage.class).getExecStateId()));
+                }
+                if (logger.isDebugEnabled()) {
+                    logger.debug("Dispatch end with bulk number: " + bulkNumber);
+                }
+            }
+        });
+        if (logger.isDebugEnabled()) {
+            logger.debug("bulk was drained in " + (System.currentTimeMillis() - t) + " ms");
+        }
+    }
 
-	@Override
-	public int getSize() {
-		return buffer.size();
-	}
+    @Override
+    public int getSize() {
+        return buffer.size();
+    }
 
-	@Override
-	public int getWeight() {
-		return currentWeight;
-	}
+    @Override
+    public int getWeight() {
+        return currentWeight;
+    }
 
     @Override
     public int getCapacity() {
@@ -228,30 +251,33 @@ public class OutboundBufferImpl implements OutboundBuffer, WorkerRecoveryListene
     }
 
     @Override
-	public String getStatus() {
-		return "Buffer status: [W:" + currentWeight + '/' + maxBufferWeight + ",S:" + buffer.size() + "]";
-	}
+    public String getStatus() {
+        return "Buffer status: [W:" + currentWeight + '/' + maxBufferWeight + ",S:" + buffer.size() + "]";
+    }
 
-	@Override
+    @Override
     public void doRecovery() {
-        if (logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("OutboundBuffer is in recovery, clearing buffer.");
         }
         buffer.clear();
-        currentWeight = 0 ;
+        currentWeight = 0;
     }
 
-    private class CompoundMessage implements Message{
+    private class CompoundMessage implements Message {
+
         private Message[] messages;
 
-        public CompoundMessage(Message[] messages){
+        public CompoundMessage(Message[] messages) {
             this.messages = messages.clone();
         }
 
         @Override
         public int getWeight() {
             int weight = 0;
-            for (Message message : messages) weight += message.getWeight();
+            for (Message message : messages) {
+                weight += message.getWeight();
+            }
             return weight;
         }
 
@@ -273,9 +299,15 @@ public class OutboundBufferImpl implements OutboundBuffer, WorkerRecoveryListene
 
     private int defaultBufferCapacity() {
         Long maxMemory = Runtime.getRuntime().maxMemory();
-        if(maxMemory  < 0.5*GB) return 10000;
-        if(maxMemory  < 1*GB) return 15000;
-        if(maxMemory  < 2*GB) return 30000;
+        if (maxMemory < 0.5 * GB) {
+            return 10000;
+        }
+        if (maxMemory < 1 * GB) {
+            return 15000;
+        }
+        if (maxMemory < 2 * GB) {
+            return 30000;
+        }
         return 60000;
     }
 }
