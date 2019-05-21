70a9340aa5176a3c4798d0760524e36487004c64
Lucian Musca <lucian-cristian.musca@hpe.com>
2019-05-21 17:18:55 +0300

    Lz4 for Execution Messages (#196)
    * Lz4 for Execution Messages.
    
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>
    
    * Fixing wrong comment.
    
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>
    
    * Fixing name.
    
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- engine/queue/score-queue-api/src/main/java/io/cloudslang/engine/queue/entities/ExecutionMessageConverter.java
+++ engine/queue/score-queue-api/src/main/java/io/cloudslang/engine/queue/entities/ExecutionMessageConverter.java
@@ -16,124 +16,109 @@
 
 package io.cloudslang.engine.queue.entities;
 
-import org.apache.commons.io.IOUtils;
 import io.cloudslang.score.facade.entities.Execution;
+import net.jpountz.lz4.LZ4FrameInputStream;
+import net.jpountz.lz4.LZ4FrameOutputStream;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-/**
- * Created with IntelliJ IDEA.
- * User: kravtsov
- * Date: 20/11/12
- * Time: 14:35
- */
+
 public class ExecutionMessageConverter {
+    private static final int SIZE = 1024;
 
-	public static final int SIZE = 1024;
-	@Autowired(required = false)
-	private SensitiveDataHandler sensitiveDataHandler;
+    @Autowired(required = false)
+    private SensitiveDataHandler sensitiveDataHandler;
 
-	public <T> T extractExecution(Payload payload) {
-		return objFromBytes(payload.getData());
-	}
+    public <T> T extractExecution(Payload payload) {
+        return objFromBytes(payload.getData());
+    }
 
-	public Payload createPayload(Execution execution) {
-		return createPayload(execution, false);
-	}
+    public Payload createPayload(Execution execution) {
+        return createPayload(execution, false);
+    }
 
-	public Payload createPayload(Execution execution, boolean setContainsSensitiveData) {
-		Payload payload = new Payload(objToBytes(execution));
-		if(setContainsSensitiveData || checkContainsSensitiveData(execution)) {
-			setSensitive(payload);
-		}
-		return payload;
-	}
+    public Payload createPayload(Execution execution, boolean setContainsSensitiveData) {
+        Payload payload = new Payload(objToBytes(execution));
+        if (setContainsSensitiveData || checkContainsSensitiveData(execution)) {
+            setSensitive(payload);
+        }
+        return payload;
+    }
 
-	private boolean checkContainsSensitiveData(Execution execution) {
-		return sensitiveDataHandler != null &&
-				sensitiveDataHandler.containsSensitiveData(execution.getSystemContext(), execution.getContexts());
-	}
+    private boolean checkContainsSensitiveData(Execution execution) {
+        return sensitiveDataHandler != null &&
+                sensitiveDataHandler.containsSensitiveData(execution.getSystemContext(), execution.getContexts());
+    }
 
-	public boolean containsSensitiveData(Payload payload) {
-		return isSensitive(payload);
-	}
+    public boolean containsSensitiveData(Payload payload) {
+        return isSensitive(payload);
+    }
 
-	private <T> T objFromBytes(byte[] bytes) {
-		ObjectInputStream ois = null;
-		try {
-			//2 Buffers are added to increase performance
-			ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+    private <T> T objFromBytes(byte[] bytes) {
+        ObjectInputStream ois = null;
+        try {
+            ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+            skipPayloadMetaData(is);
 
-			skipPayloadMetaData(is);
+            ois = new ObjectInputStream(new LZ4FrameInputStream(is));
+            // noinspection unchecked
+            return (T) ois.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            throw new RuntimeException("Failed to read execution plan from byte[]. Error: ", ex);
+        } finally {
+            IOUtils.closeQuietly(ois);
+        }
+    }
 
-			BufferedInputStream bis = new BufferedInputStream(is);
-			ois = new ObjectInputStream(bis);
+    private byte[] objToBytes(Object obj) {
+        ObjectOutputStream oos = null;
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(SIZE);
+            initPayloadMetaData(baos);
 
-			//noinspection unchecked
-			return (T)ois.readObject();
-		}
-		catch(IOException | ClassNotFoundException ex) {
-			throw new RuntimeException("Failed to read execution plan from byte[]. Error: ", ex);
-		}
-		finally {
-			IOUtils.closeQuietly(ois);
-		}
+            oos = new ObjectOutputStream(new LZ4FrameOutputStream(baos));
+            oos.writeObject(obj);
+            oos.flush();
 
-	}
+            return baos.toByteArray();
+        } catch (IOException ex) {
+            throw new RuntimeException("Failed to serialize execution plan. Error: ", ex);
+        } finally {
+            IOUtils.closeQuietly(oos);
+        }
+    }
 
-	private byte[] objToBytes(Object obj){
-		ObjectOutputStream oos = null;
-		try {
-			ByteArrayOutputStream bout = new ByteArrayOutputStream(SIZE);
+    /***************************************************************************************/
+    //we padding payload with clean bytes which then will be used for metadata writing
+    private static final byte[] PAYLOAD_META_DATA_INIT_BYTES = {0};
 
-			initPayloadMetaData(bout);
+    //for now meta data is only one byte
+    private static final int INFRA_PART_BYTE = 0;
 
-			BufferedOutputStream bos = new BufferedOutputStream(bout);
-			oos = new ObjectOutputStream(bos);
+    private static final int IS_SENSITIVE = 1;
 
-			oos.writeObject(obj);
-			oos.flush();
+    private void setSensitive(Payload payload) {
+        payload.getData()[INFRA_PART_BYTE] = IS_SENSITIVE;
+    }
 
-			return bout.toByteArray();
-		}
-		catch(IOException ex) {
-			throw new RuntimeException("Failed to serialize execution plan. Error: ", ex);
-		} finally {
-			IOUtils.closeQuietly(oos);
-		}
-	}
+    private boolean isSensitive(Payload payload) {
+        return payload.getData()[INFRA_PART_BYTE] == IS_SENSITIVE;
+    }
 
-	/***************************************************************************************/
-	//we padding payload with clean bytes which then will be used for metadata writing
-	private static final byte[] PAYLOAD_META_DATA_INIT_BYTES = {0};
+    private void skipPayloadMetaData(ByteArrayInputStream is) {
+        for (int i = 0; i < PAYLOAD_META_DATA_INIT_BYTES.length; i++) {
+            is.read();
+        }
+    }
 
-	//for now meta data is only one byte
-	private static final int INFRA_PART_BYTE = 0;
+    private void initPayloadMetaData(ByteArrayOutputStream baos) throws IOException {
+        baos.write(PAYLOAD_META_DATA_INIT_BYTES);
+    }
 
-	private static final int IS_SENSITIVE = 1;
-
-	private void setSensitive(Payload payload) {
-		payload.getData()[INFRA_PART_BYTE] = IS_SENSITIVE;
-	}
-
-	private boolean isSensitive(Payload payload) {
-		return payload.getData()[INFRA_PART_BYTE] == IS_SENSITIVE;
-	}
-
-	private void skipPayloadMetaData(ByteArrayInputStream is) throws IOException {
-		for(int i = 0; i < PAYLOAD_META_DATA_INIT_BYTES.length; i++) {
-			is.read();
-		}
-	}
-
-	private void initPayloadMetaData(ByteArrayOutputStream baos) throws IOException {
-		baos.write(PAYLOAD_META_DATA_INIT_BYTES);
-	}
 }
