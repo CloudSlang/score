46e62545c796ddb6e6aaabf88fd098d36ca85b07
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-05-29 14:11:32 +0300

    Revert back
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/SimpleExecutionRunnableFactory.java
+++ worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/SimpleExecutionRunnableFactory.java
@@ -16,31 +16,37 @@
 
 package io.cloudslang.worker.management.services;
 
+import io.cloudslang.worker.execution.services.ExecutionService;
 import io.cloudslang.engine.queue.entities.ExecutionMessageConverter;
 import io.cloudslang.engine.queue.services.QueueStateIdGeneratorService;
-import io.cloudslang.worker.execution.services.ExecutionService;
+
 import io.cloudslang.worker.management.WorkerConfigurationService;
 import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Resource;
 
+/**
+ * Created by IntelliJ IDEA.
+ * User:
+ * Date: 19/12/12
+ */
 public class SimpleExecutionRunnableFactory implements FactoryBean<SimpleExecutionRunnable> {
 
-    @Autowired
-    private ExecutionService executionService;
+	@Autowired
+	private ExecutionService executionService;
+
+	@Autowired
+	private OutboundBuffer outBuffer;
 
     @Autowired
-    private OutboundBuffer outBuffer;
+   	private InBuffer inBuffer;
 
-    @Autowired
-    private InBuffer inBuffer;
+	@Autowired
+	private ExecutionMessageConverter converter;
 
-    @Autowired
-    private ExecutionMessageConverter converter;
-
-    @Autowired
-    private EndExecutionCallback endExecutionCallback;
+	@Autowired
+	private EndExecutionCallback endExecutionCallback;
 
     @Autowired
     private QueueStateIdGeneratorService queueStateIdGeneratorService;
@@ -51,15 +57,12 @@ public class SimpleExecutionRunnableFactory implements FactoryBean<SimpleExecuti
     @Autowired
     private WorkerManager workerManager;
 
-    @Autowired
-    private SimpleRunnableContinuation simpleRunnableContinuation;
-
     @Resource
-    private String workerUuid;
+	private String workerUuid;
 
-    @Override
-    public SimpleExecutionRunnable getObject() {
-        return new SimpleExecutionRunnable(
+	@Override
+	public SimpleExecutionRunnable getObject() {
+		return new SimpleExecutionRunnable(
                 executionService,
                 outBuffer,
                 inBuffer,
@@ -68,18 +71,17 @@ public class SimpleExecutionRunnableFactory implements FactoryBean<SimpleExecuti
                 queueStateIdGeneratorService,
                 workerUuid,
                 workerConfigurationService,
-                workerManager,
-                simpleRunnableContinuation
+                workerManager
         );
-    }
+	}
 
-    @Override
-    public Class<?> getObjectType() {
-        return SimpleExecutionRunnable.class;
-    }
+	@Override
+	public Class<?> getObjectType() {
+		return SimpleExecutionRunnable.class;
+	}
 
-    @Override
-    public boolean isSingleton() {
-        return false;
-    }
+	@Override
+	public boolean isSingleton() {
+		return false;
+	}
 }
