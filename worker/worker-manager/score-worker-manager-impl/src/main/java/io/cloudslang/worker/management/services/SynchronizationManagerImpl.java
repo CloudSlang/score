5afc132bd8009d7f790b8d49d1909c44fa67c31a
Lucian Musca <lucian-cristian.musca@microfocus.com>
2019-05-31 10:03:25 +0300

    Remove of some useless logging statements
    Signed-off-by: Lucian Musca <lucian-cristian.musca@microfocus.com>

--- worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/SynchronizationManagerImpl.java
+++ worker/worker-manager/score-worker-manager-impl/src/main/java/io/cloudslang/worker/management/services/SynchronizationManagerImpl.java
@@ -21,46 +21,36 @@ import org.apache.log4j.Logger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
-/**
- * Created with IntelliJ IDEA.
- * User: kravtsov
- * Date: 8/7/14
- * Time: 2:30 PM
- */
 public class SynchronizationManagerImpl implements SynchronizationManager {
+    private static final Logger logger = Logger.getLogger(SynchronizationManagerImpl.class);
 
-    private final Logger logger = Logger.getLogger(this.getClass());
+    private final ReentrantLock recoveryGetLock = new ReentrantLock();  //synchronizing Recovery and InBuffer
+    private final ReentrantLock recoveryPutLock = new ReentrantLock(); //synchronizing Recovery and OutBuffer put()
+    private final ReentrantLock recoveryDrainLock = new ReentrantLock(); //synchronizing Recovery and OutBuffer drain()
+    private final ReentrantLock outBufferLock = new ReentrantLock();   //synchronizing OutBuffer put() and drain()
 
-    ReentrantLock recoveryGetLock = new ReentrantLock();  //synchronizing Recovery and InBuffer
-    ReentrantLock recoveryPutLock = new ReentrantLock(); //synchronizing Recovery and OutBuffer put()
-    ReentrantLock recoveryDrainLock = new ReentrantLock(); //synchronizing Recovery and OutBuffer drain()
-
-    ReentrantLock outBufferLock = new ReentrantLock();   //synchronizing OutBuffer put() and drain()
     private final Condition notEmpty = outBufferLock.newCondition();
     private final Condition notFull = outBufferLock.newCondition();
 
-
     @Override
     public void startRecovery() {
-
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Getting recovery locks...");
         }
 
         recoveryGetLock.lock();
         recoveryPutLock.lock();
         recoveryDrainLock.lock();
-
         outBufferLock.lock();
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Got recovery locks...");
         }
     }
 
     @Override
-    public void finishRecovery(){
-        if(logger.isDebugEnabled()){
+    public void finishRecovery() {
+        if (logger.isDebugEnabled()) {
             logger.debug("Releasing recovery locks...");
         }
 
@@ -70,59 +60,42 @@ public class SynchronizationManagerImpl implements SynchronizationManager {
         recoveryGetLock.unlock();
         recoveryPutLock.unlock();
         recoveryDrainLock.unlock();
-
         outBufferLock.unlock();
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Released recovery locks...");
         }
     }
 
     @Override
     public void startPutMessages() {
-        if(logger.isDebugEnabled()){
-            logger.debug("In SynchronizationManager.startPutMessages()");
-        }
-
         recoveryPutLock.lock();
         outBufferLock.lock();
-
-        if(logger.isDebugEnabled()){
-            logger.debug("Out SynchronizationManager.startPutMessages()");
-        }
     }
 
     @Override
     public void finishPutMessages() {
-        if(logger.isDebugEnabled()){
-            logger.debug("In SynchronizationManager.finishPutMessages()");
-        }
         notEmpty.signalAll();
-
         unlockCompletely(recoveryPutLock);
-        unlockCompletely(outBufferLock); //probably just unlock is enough
-
-        if(logger.isDebugEnabled()){
-            logger.debug("Out SynchronizationManager.finishPutMessages()");
-        }
+        unlockCompletely(outBufferLock); // probably just unlock is enough
     }
 
     @Override
     public void startDrain() {
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("In SynchronizationManager.startDrain()");
         }
         recoveryDrainLock.lock();
         outBufferLock.lock();
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Out SynchronizationManager.startDrain()");
         }
     }
 
     @Override
     public void finishDrain() {
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("In SynchronizationManager.finishDrain()");
         }
 
@@ -131,15 +104,14 @@ public class SynchronizationManagerImpl implements SynchronizationManager {
         unlockCompletely(recoveryDrainLock);
         unlockCompletely(outBufferLock); //probably just unlock is enough
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Out SynchronizationManager.finishDrain()");
         }
     }
 
     @Override
     public void waitForDrain() throws InterruptedException {
-
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("In SynchronizationManager.waitForDrain()");
         }
 
@@ -150,7 +122,7 @@ public class SynchronizationManagerImpl implements SynchronizationManager {
         //Wait for messages to be drained
         notFull.await();
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Out SynchronizationManager.waitForDrain()");
         }
 
@@ -158,8 +130,7 @@ public class SynchronizationManagerImpl implements SynchronizationManager {
 
     @Override
     public void waitForMessages() throws InterruptedException {
-
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("In SynchronizationManager.waitForMessages()");
         }
 
@@ -168,33 +139,33 @@ public class SynchronizationManagerImpl implements SynchronizationManager {
         //Wait for messages to arrive
         notEmpty.await();
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Out SynchronizationManager.waitForMessages()");
         }
     }
 
     @Override
     public void startGetMessages() {
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("In SynchronizationManager.startGetMessages()");
         }
 
         recoveryGetLock.lock();
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Out SynchronizationManager.startGetMessages()");
         }
     }
 
     @Override
     public void finishGetMessages() {
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("In SynchronizationManager.finishGetMessages()");
         }
 
         unlockCompletely(recoveryGetLock);
 
-        if(logger.isDebugEnabled()){
+        if (logger.isDebugEnabled()) {
             logger.debug("Out SynchronizationManager.finishGetMessages()");
         }
     }
@@ -202,10 +173,10 @@ public class SynchronizationManagerImpl implements SynchronizationManager {
     // It is very important to use this method instead of just do unlock because of 2 reasons:
     // 1. The lock could be already unlocked in case out thread was in waitForMessages() or waitForDrain()
     //2. The lock can be locked more then once - for example in InBuffer startGetMessages() and then in ackMessages() do put() in OutBuffer and do startPutMessages()
-    private void unlockCompletely(ReentrantLock lockToUnlock){
+    private void unlockCompletely(ReentrantLock lockToUnlock) {
         int counter = lockToUnlock.getHoldCount();
 
-        for(int i = 0; i<counter; i++){
+        for (int i = 0; i < counter; i++) {
             lockToUnlock.unlock();
         }
     }
