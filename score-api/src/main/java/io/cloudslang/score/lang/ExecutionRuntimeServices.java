3b8ac4db1c2cd658891cae671fbceb72610b36ad
Adriana-Natalia Corui <adriana-natalia.corui@microfocus.com>
2019-09-24 13:03:23 +0300

    check trigger type

--- score-api/src/main/java/io/cloudslang/score/lang/ExecutionRuntimeServices.java
+++ score-api/src/main/java/io/cloudslang/score/lang/ExecutionRuntimeServices.java
@@ -95,6 +95,8 @@ public class ExecutionRuntimeServices implements Serializable {
 
     private static final String PRECONDITION_NOT_FULFILLED = "PRECONDITION_NOT_FULFILLED";
 
+    private static final String TRIGGER_TYPE = "TRIGGER_TYPE";
+
     protected Map<String, Serializable> contextMap = new HashMap<>();
 
     public ExecutionRuntimeServices() {
@@ -489,6 +491,14 @@ public class ExecutionRuntimeServices implements Serializable {
 
     public boolean getPreconditionNotFulfilled() {
         return getFromMap(PRECONDITION_NOT_FULFILLED) != null;
+    }
+
+    public void setTriggerType(String triggerType) {
+        contextMap.put(TRIGGER_TYPE, triggerType);
+    }
+
+    public String getTriggerType() {
+        return getFromMap(TRIGGER_TYPE);
     }
 
     private <T> T removeFromMap(String key) {
