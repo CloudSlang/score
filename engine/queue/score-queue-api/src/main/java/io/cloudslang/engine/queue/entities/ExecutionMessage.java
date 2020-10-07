b2b4f83548bc3aa4d4ac594fb15ffa25a36cd888
Lucian Musca <lucian-cristian.musca@microfocus.com>
2020-10-07 14:08:35 +0300

    Fix ClassCastException

--- engine/queue/score-queue-api/src/main/java/io/cloudslang/engine/queue/entities/ExecutionMessage.java
+++ engine/queue/score-queue-api/src/main/java/io/cloudslang/engine/queue/entities/ExecutionMessage.java
@@ -28,52 +28,50 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * User:
- * Date: 10/09/12
- * Time: 11:11
+ * User: Date: 10/09/12 Time: 11:11
  */
 public class ExecutionMessage implements Message, Cloneable {
 
     private static final long serialVersionUID = 3523623124812765964L;
 
-	private static final int JVM_OBJECT_HEADER = 16;
-	private static final int ARRAY_LENGTH_FIELD = 4;
-	private static final int PADDING_LENGTH = 4;
+    private static final int JVM_OBJECT_HEADER = 16;
+    private static final int ARRAY_LENGTH_FIELD = 4;
+    private static final int PADDING_LENGTH = 4;
 
-	public static final long EMPTY_EXEC_STATE_ID = -1L;
-	public static final String EMPTY_WORKER = "EMPTY";
+    public static final long EMPTY_EXEC_STATE_ID = -1L;
+    public static final String EMPTY_WORKER = "EMPTY";
 
-	private long execStateId;
-	private String workerId;
-	private String workerGroup;
-	private ExecStatus status;
-	private Payload payload;
-	private long payloadSize;	//in bytes
-	private int msgSeqId;
-	private String msgId;
+    private long execStateId;
+    private String workerId;
+    private String workerGroup;
+    private ExecStatus status;
+    private Payload payload;
+    private long payloadSize;    //in bytes
+    private int msgSeqId;
+    private String msgId;
     private Long createDate;
 
-	private boolean stepPersist;
-	private String stepPersistId;
+    private boolean stepPersist;
+    private String stepPersistId;
 
-	private transient String workerKey;
+    private transient String workerKey;
 
     private transient Execution executionObject;
 
     private boolean active;
 
-	public ExecutionMessage() {
-		execStateId = EMPTY_EXEC_STATE_ID;
-		workerId = ExecutionMessage.EMPTY_WORKER;
-		workerGroup = "";
-		status = ExecStatus.INIT;
-		payload = null;
-		payloadSize = 0;
-		msgSeqId = -1;
-		msgId = "";
+    public ExecutionMessage() {
+        execStateId = EMPTY_EXEC_STATE_ID;
+        workerId = ExecutionMessage.EMPTY_WORKER;
+        workerGroup = "";
+        status = ExecStatus.INIT;
+        payload = null;
+        payloadSize = 0;
+        msgSeqId = -1;
+        msgId = "";
         createDate = null;
         active = true;
-	}
+    }
 
     public ExecutionMessage(String executionId, Payload payload) {
         this.execStateId = ExecutionMessage.EMPTY_EXEC_STATE_ID;
@@ -88,51 +86,51 @@ public class ExecutionMessage implements Message, Cloneable {
     }
 
     public ExecutionMessage(long execStateId,
-    	                        String workerId,
-    	                        String workerGroup,
-    	                        String msgId,
-    	                        ExecStatus status,
-    	                        Payload payload,
-    	                        int msgSeqId,
-                                Long createDate) {
-		this.execStateId = execStateId;
-		this.workerId = workerId;
-		this.workerGroup = workerGroup;
-		this.msgId = msgId;
-		this.status = status;
-		this.payload = payload;
-		this.payloadSize = getPayloadSize(payload);
-		this.msgSeqId = msgSeqId;
-		this.createDate = createDate;
-		this.active = true;
-   }
-
-	public ExecutionMessage(long execStateId,
-	                        String workerId,
-	                        String workerGroup,
-	                        String msgId,
-	                        ExecStatus status,
-	                        Payload payload,
-	                        int msgSeqId) {
-		this.execStateId = execStateId;
-		this.workerId = workerId;
-		this.workerGroup = workerGroup;
-		this.msgId = msgId;
-		this.status = status;
-		this.payload = payload;
-		this.payloadSize = getPayloadSize(payload);
-		this.msgSeqId = msgSeqId;
-		this.active = true;
-	}
+            String workerId,
+            String workerGroup,
+            String msgId,
+            ExecStatus status,
+            Payload payload,
+            int msgSeqId,
+            Long createDate) {
+        this.execStateId = execStateId;
+        this.workerId = workerId;
+        this.workerGroup = workerGroup;
+        this.msgId = msgId;
+        this.status = status;
+        this.payload = payload;
+        this.payloadSize = getPayloadSize(payload);
+        this.msgSeqId = msgSeqId;
+        this.createDate = createDate;
+        this.active = true;
+    }
 
     public ExecutionMessage(long execStateId,
-                            String workerId,
-                            String workerGroup,
-                            String msgId,
-                            ExecStatus status,
-                            Execution executionObject,
-                            Payload payload,
-                            int msgSeqId) {
+            String workerId,
+            String workerGroup,
+            String msgId,
+            ExecStatus status,
+            Payload payload,
+            int msgSeqId) {
+        this.execStateId = execStateId;
+        this.workerId = workerId;
+        this.workerGroup = workerGroup;
+        this.msgId = msgId;
+        this.status = status;
+        this.payload = payload;
+        this.payloadSize = getPayloadSize(payload);
+        this.msgSeqId = msgSeqId;
+        this.active = true;
+    }
+
+    public ExecutionMessage(long execStateId,
+            String workerId,
+            String workerGroup,
+            String msgId,
+            ExecStatus status,
+            Execution executionObject,
+            Payload payload,
+            int msgSeqId) {
         this.execStateId = execStateId;
         this.workerId = workerId;
         this.workerGroup = workerGroup;
@@ -145,23 +143,23 @@ public class ExecutionMessage implements Message, Cloneable {
         this.active = true;
     }
 
-	public boolean isStepPersist() {
-		return stepPersist;
-	}
+    public boolean isStepPersist() {
+        return stepPersist;
+    }
 
-	public void setStepPersist(boolean stepPersist) {
-		this.stepPersist = stepPersist;
-	}
+    public void setStepPersist(boolean stepPersist) {
+        this.stepPersist = stepPersist;
+    }
 
-	public String getStepPersistId() {
-		return stepPersistId;
-	}
+    public String getStepPersistId() {
+        return stepPersistId;
+    }
 
-	public void setStepPersistId(String stepPersistId) {
-		this.stepPersistId = stepPersistId;
-	}
+    public void setStepPersistId(String stepPersistId) {
+        this.stepPersistId = stepPersistId;
+    }
 
-	public Execution getExecutionObject() {
+    public Execution getExecutionObject() {
         return executionObject;
     }
 
@@ -177,42 +175,42 @@ public class ExecutionMessage implements Message, Cloneable {
         this.createDate = createDate;
     }
 
-	public long getExecStateId() {
-		return execStateId;
-	}
+    public long getExecStateId() {
+        return execStateId;
+    }
 
-	public void setExecStateId(long id) {
-		this.execStateId = id;
-	}
+    public void setExecStateId(long id) {
+        this.execStateId = id;
+    }
 
-	public String getMsgId() {
-		return msgId;
-	}
+    public String getMsgId() {
+        return msgId;
+    }
 
-	public void setMsgId(String msg_id) {
-		this.msgId = msg_id;
-	}
+    public void setMsgId(String msg_id) {
+        this.msgId = msg_id;
+    }
 
-	public String getWorkerId() {
-		return workerId;
-	}
+    public String getWorkerId() {
+        return workerId;
+    }
 
-	public String getWorkerGroup() {
-		return workerGroup;
-	}
+    public String getWorkerGroup() {
+        return workerGroup;
+    }
 
-	@JsonIgnore
-	public String getMsgUniqueId() {
-		return msgId + ":" + msgSeqId;
-	}
+    @JsonIgnore
+    public String getMsgUniqueId() {
+        return msgId + ":" + msgSeqId;
+    }
 
-	public ExecStatus getStatus() {
-		return status;
-	}
+    public ExecStatus getStatus() {
+        return status;
+    }
 
-	public Payload getPayload() {
-		return payload;
-	}
+    public Payload getPayload() {
+        return payload;
+    }
 
     public void setWorkerGroup(String workerGroup) {
         this.workerGroup = workerGroup;
@@ -220,68 +218,68 @@ public class ExecutionMessage implements Message, Cloneable {
 
     public void setPayload(Payload payload) {
         this.payload = payload;
-		this.payloadSize = getPayloadSize(payload) + JVM_OBJECT_HEADER + ARRAY_LENGTH_FIELD + PADDING_LENGTH;
+        this.payloadSize = getPayloadSize(payload) + JVM_OBJECT_HEADER + ARRAY_LENGTH_FIELD + PADDING_LENGTH;
     }
 
-	public long getPayloadSize() {
-		return payloadSize;
-	}
+    public long getPayloadSize() {
+        return payloadSize;
+    }
 
-	public void setPayloadSize(long payloadSize) {
-		this.payloadSize = payloadSize;
-	}
+    public void setPayloadSize(long payloadSize) {
+        this.payloadSize = payloadSize;
+    }
 
-	public int getMsgSeqId() {
-		return msgSeqId;
-	}
+    public int getMsgSeqId() {
+        return msgSeqId;
+    }
 
-	public void setStatus(ExecStatus status) {
-		this.status = status;
-	}
+    public void setStatus(ExecStatus status) {
+        this.status = status;
+    }
 
-	public synchronized void incMsgSeqId() {
-		this.msgSeqId = msgSeqId + 1;
-	}
+    public synchronized void incMsgSeqId() {
+        this.msgSeqId = msgSeqId + 1;
+    }
 
-	public void setWorkerId(String workerId) {
-		this.workerId = workerId;
-	}
+    public void setWorkerId(String workerId) {
+        this.workerId = workerId;
+    }
 
-	@Override
-	public int getWeight() {
-		return 1;
-	}
+    @Override
+    public int getWeight() {
+        return 1;
+    }
 
-	@Override
-	public String getId() {
-		return workerKey;
-	}
+    @Override
+    public String getId() {
+        return workerKey;
+    }
 
-	public String getWorkerKey() {
-		return workerKey;
-	}
+    public String getWorkerKey() {
+        return workerKey;
+    }
 
-	public boolean isActive() {
-		return active;
-	}
+    public boolean isActive() {
+        return active;
+    }
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 
-	public ExecutionMessage setWorkerKey(String workerKey) {
-		this.workerKey = workerKey;
-		return this;
-	}
+    public ExecutionMessage setWorkerKey(String workerKey) {
+        this.workerKey = workerKey;
+        return this;
+    }
 
-	private int getPayloadSize(Payload payload) {
-		return payload != null ? payload.getData().length : 0;
-	}
+    private int getPayloadSize(Payload payload) {
+        return payload != null ? payload.getData().length : 0;
+    }
 
-	@Override
-	public List<Message> shrink(List<Message> messages) {
-		if (messages.size() > 2) {
-			List<Message> resultAfterShrink = new ArrayList<>();
+    @Override
+    public List<Message> shrink(List<Message> messages) {
+        if (messages.size() > 2) {
+            List<Message> resultAfterShrink = new ArrayList<>();
 
 			ExecutionMessage firstMessage  = (ExecutionMessage) messages.get(0);
             ExecutionMessage secondMessage = (ExecutionMessage) messages.get(1);
@@ -330,73 +328,79 @@ public class ExecutionMessage implements Message, Cloneable {
     private String messagesToString(List<Message> messages){
         StringBuilder str = new StringBuilder();
 
-        for(Message m : messages){
+        for (Message m : messages) {
             str.append(m.toString()).append("\n");
         }
 
-       return str.toString();
+        return str.toString();
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder str = new StringBuilder();
 
         boolean isAck = this.getExecutionObject() == null;
 
         str.append(" ExecutionId:").append(this.msgId).
-        append(" ExecStateId:").append(this.execStateId).
-        append(" Status:").append(this.status).
-        append(" WorkerKey:").append(this.getId()).
+                append(" ExecStateId:").append(this.execStateId).
+                append(" Status:").append(this.status).
+                append(" WorkerKey:").append(this.getId()).
                 append(" IsAck:").append(isAck)
-		.append(" IsActive:").append(active);
+                .append(" IsActive:").append(active);
 
         return str.toString();
     }
 
-	@SuppressWarnings("CloneDoesntDeclareCloneNotSupportedException")
-	@Override
-	public Object clone() {
-		try {
-			ExecutionMessage cloned = (ExecutionMessage) super.clone();
-			if (payload != null) cloned.payload = (Payload) (payload.clone());
-			return cloned;
-		} catch (CloneNotSupportedException ex) {
-			throw new RuntimeException("Failed to clone message", ex);
+    @SuppressWarnings("CloneDoesntDeclareCloneNotSupportedException")
+    @Override
+    public Object clone() {
+        try {
+            ExecutionMessage cloned = (ExecutionMessage) super.clone();
+			if (payload != null) {
+				cloned.payload = (Payload) (payload.clone());
+			}
+            return cloned;
+        } catch (CloneNotSupportedException ex) {
+            throw new RuntimeException("Failed to clone message", ex);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+		if (this == o) {
+			return true;
 		}
-	}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-
-		ExecutionMessage that = (ExecutionMessage) o;
-		return new EqualsBuilder()
-				.append(this.execStateId, that.execStateId)
-				.append(this.msgSeqId, that.msgSeqId)
-				.append(this.msgId, that.msgId)
-				.append(this.payload, that.payload)
-				.append(this.payloadSize, that.payloadSize)
-				.append(this.status, that.status)
-				.append(this.workerGroup, that.workerGroup)
-				.append(this.workerId, that.workerId)
+        ExecutionMessage that = (ExecutionMessage) o;
+        return new EqualsBuilder()
+                .append(this.execStateId, that.execStateId)
+                .append(this.msgSeqId, that.msgSeqId)
+                .append(this.msgId, that.msgId)
+                .append(this.payload, that.payload)
+                .append(this.payloadSize, that.payloadSize)
+                .append(this.status, that.status)
+                .append(this.workerGroup, that.workerGroup)
+                .append(this.workerId, that.workerId)
                 .append(this.createDate, that.createDate)
-				.append(this.active, that.active)
-				.isEquals();
-	}
+                .append(this.active, that.active)
+                .isEquals();
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(
-				workerId,
-				workerGroup,
-				msgId,
-				status,
-				payload,
-				msgSeqId,
-				execStateId,
+    @Override
+    public int hashCode() {
+        return Objects.hash(
+                workerId,
+                workerGroup,
+                msgId,
+                status,
+                payload,
+                msgSeqId,
+                execStateId,
                 createDate,
-				active
-		);
-	}
+                active
+        );
+    }
 }
