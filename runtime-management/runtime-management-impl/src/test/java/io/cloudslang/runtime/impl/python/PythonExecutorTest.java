f835909292bebb37f688c16da9fa13a2cc0b0974
opreateodora <74269902+opreateodora@users.noreply.github.com>
2023-01-12 11:56:36 +0200

    Python Executor (#445)
    * refactor code
    
    * change name pythonServer in pythonExecutor
    
    Co-authored-by: TVerdesOprea <teodora.verdesoprea@microfocus.com>

--- runtime-management/runtime-management-impl/src/test/java/io/cloudslang/runtime/impl/python/PythonExecutorTest.java
+++ runtime-management/runtime-management-impl/src/test/java/io/cloudslang/runtime/impl/python/PythonExecutorTest.java
@@ -25,9 +25,8 @@ import io.cloudslang.runtime.api.python.PythonEvaluationResult;
 import io.cloudslang.runtime.api.python.PythonExecutionResult;
 import io.cloudslang.runtime.api.python.PythonRuntimeService;
 import io.cloudslang.runtime.impl.python.external.ExternalPythonExecutionEngine;
+import io.cloudslang.runtime.impl.python.external.ExternalPythonExecutorServiceImpl;
 import io.cloudslang.runtime.impl.python.external.ExternalPythonRuntimeServiceImpl;
-import io.cloudslang.runtime.impl.python.external.ExternalPythonServerService;
-import io.cloudslang.runtime.impl.python.external.ExternalPythonServerServiceImpl;
 import io.cloudslang.runtime.impl.python.external.StatefulRestEasyClientsHolder;
 import io.cloudslang.score.events.EventBus;
 import org.junit.Assert;
@@ -332,9 +331,10 @@ public class PythonExecutorTest {
             return new PythonRuntimeServiceImpl();
         }
 
-        @Bean(name = "externalPythonServerService")
-        public ExternalPythonServerService externalPythonServerService() {
-            return new ExternalPythonServerServiceImpl(mock(StatefulRestEasyClientsHolder.class));
+        @Bean(name = "externalPythonExecutorService")
+        public PythonRuntimeService externalPythonExecutorService() {
+            return new ExternalPythonExecutorServiceImpl(mock(StatefulRestEasyClientsHolder.class),
+                    new Semaphore(100), new Semaphore(50));
         }
 
         @Bean(name = "externalPythonRuntimeService")
